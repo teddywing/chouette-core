48c8997c649fa183923164714ea916c4e6976727
Xinhui <xinhui.xu@af83.com>
2016-05-13 18:27:51 +0200

    Tempset users.username to nullable

--- db/migrate/20160511134824_add_username_to_users.rb
+++ db/migrate/20160511134824_add_username_to_users.rb
@@ -1,15 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration
-  def up
-    add_column :users, :username, :string, :null => false
+  def change
+    add_column :users, :username, :string, :null => true
     add_index :users, :username, :unique => true
-    User.all.each do |u|
-      u.username = u.email
-      u.save
-    end
-  end
-
-  def down
-    remove_column :users, :username, :string, :null => false
-    remove_index :users, :username, :unique => true
   end
 end
