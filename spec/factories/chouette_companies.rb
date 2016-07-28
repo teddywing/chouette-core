c73fc634eee6e306594df37f7ec21be7a6366dbd
Edouard Maffert <edouard.maffert@af83.com>
2016-07-28 15:05:14 +0200

    fix tests

--- spec/factories/chouette_companies.rb
+++ spec/factories/chouette_companies.rb
@@ -2,8 +2,10 @@ FactoryGirl.define do
 
   factory :company, :class => Chouette::Company do
     sequence(:name) { |n| "Company #{n}" }
-    sequence(:objectid) { |n| "test:Company:#{n}" }
+    sequence(:objectid) { |n| "chouette:test:Company:#{n}" }
     sequence(:registration_number) { |n| "test-#{n}" }
+
+    association :line_referential, :factory => :line_referential
   end
 
 end
