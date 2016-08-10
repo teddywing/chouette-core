b39899e26ec2ab3cba7d8f30fd02ee3d06ba01e2
Xinhui <xinhui.xu@af83.com>
2016-08-10 11:07:30 +0200

    Refactoring rspec portail sync code into model spec

--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Organisation do
+describe Organisation, :type => :model do
 
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
@@ -12,5 +12,40 @@ describe Organisation do
   it "create a rule_parameter_set" do
     organisation = create(:organisation)
     organisation.rule_parameter_sets.size.should == 1
+  end
+
+  describe "Portail sync" do
+    let(:conf) { Rails.application.config.stif_portail_api }
+    before :each do
+      stub_request(:get, "#{conf[:url]}/api/v1/organizations").
+      with(headers: { 'Authorization' => "Token token=\"#{conf[:key]}\"" }).
+      to_return(body: File.open(File.join(Rails.root, 'spec', 'fixtures', 'organizations.json')), status: 200)
+    end
+
+    it 'should retrieve data from portail api' do
+      expect(Organisation.portail_api_request).to be_truthy
+      expect(WebMock).to have_requested(:get, "#{conf[:url]}/api/v1/organizations").
+        with(headers: { 'Authorization' => "Token token=\"#{conf[:key]}\"" })
+    end
+
+    it 'should create new organisations' do
+      expect{Organisation.portail_sync}.to change{ Organisation.count }.by(5)
+      expect(Organisation.all.map(&:name)).to include 'ALBATRANS', 'OPTILE', 'SNCF', 'STIF'
+    end
+
+    it 'should update existing organisations' do
+      create :organisation, name: 'dummy_name', code:'RATP', updated_at: 10.days.ago
+      Organisation.portail_sync
+      Organisation.find_by(code: 'RATP').tap do |org|
+        expect(org.name).to eq('RATP')
+        expect(org.updated_at.utc).to be_within(1.second).of Time.now
+        expect(org.synced_at.utc).to be_within(1.second).of Time.now
+      end
+    end
+
+    it 'should not create organisation if code is already present' do
+      create :organisation, code:'RATP'
+      expect{Organisation.portail_sync}.to change{ Organisation.count }.by(4)
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,6 +50,57 @@ describe User, :type => :model do
     end
   end
 
+  describe "Portail sync" do
+    let(:conf) { Rails.application.config.stif_portail_api }
+    before :each do
+      stub_request(:get, "#{conf[:url]}/api/v1/users").
+        with(headers: { 'Authorization' => "Token token=\"#{conf[:key]}\"" }).
+        to_return(body: File.open(File.join(Rails.root, 'spec', 'fixtures', 'users.json')), status: 200)
+    end
+
+    it 'should retrieve data from portail api' do
+      expect(User.portail_api_request).to be_truthy
+      expect(WebMock).to have_requested(:get, "#{conf[:url]}/api/v1/users").
+        with(headers: { 'Authorization' => "Token token=\"#{conf[:key]}\"" })
+    end
+
+    it 'should create new users' do
+      User.portail_sync
+      expect(User.count).to eq(11)
+      expect(Organisation.count).to eq(3)
+    end
+
+    it 'should update existing users' do
+      create :user, username: 'alban.peignier', email:'dummy@example.com', updated_at: 10.days.ago
+      User.portail_sync
+      user = User.find_by(username: 'alban.peignier')
+
+      expect(user.name).to eq('Alban Peignier')
+      expect(user.email).to eq('alban.peignier@af83.com')
+      expect(user.updated_at.utc).to be_within(1.second).of Time.now
+      expect(user.synced_at.utc).to be_within(1.second).of Time.now
+    end
+
+    it 'should update organisation assignement' do
+      create :user, username: 'alban.peignier', organisation: create(:organisation)
+      User.portail_sync
+      expect(User.find_by(username: 'alban.peignier').organisation.name).to eq("STIF")
+    end
+
+    it 'should update locked_at attribute' do
+      create :user, username: 'alban.peignier', locked_at: Time.now
+      User.portail_sync
+      expect(User.find_by(username: 'alban.peignier').locked_at).to be_nil
+      expect(User.find_by(username: 'jane.doe').locked_at).to eq("2016-08-05T12:34:03.995Z")
+    end
+
+    it 'should not create new user if username is already present' do
+      create :user, username: 'alban.peignier'
+      User.portail_sync
+      expect(User.count).to eq(11)
+    end
+  end
+
   describe "#destroy" do
     let!(:organisation){create(:organisation)}
     let!(:user){create(:user, :organisation => organisation)}
