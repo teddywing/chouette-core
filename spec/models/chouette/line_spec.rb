d3d7d4aea6672beae5baa3bc184303d5de109a61
Edouard Maffert <edouard.maffert@af83.com>
2016-09-21 12:09:56 +0200

    validate presence of network and companies for lines

--- spec/models/chouette/line_spec.rb
+++ spec/models/chouette/line_spec.rb
@@ -4,9 +4,8 @@ describe Chouette::Line, :type => :model do
 
   subject { create(:line) }
 
-  # FIXME #825
-  # it { is_expected.to validate_presence_of :network }
-  # it { is_expected.to validate_presence_of :company }
+  it { is_expected.to validate_presence_of :network }
+  it { is_expected.to validate_presence_of :company }
 
   it { is_expected.to validate_presence_of :name }
 
