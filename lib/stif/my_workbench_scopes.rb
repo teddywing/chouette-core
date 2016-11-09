0dbc80d54b1362c9eb0fc87d8d93f3d60ade68e1
Xinhui <xinhui.xu@af83.com>
2016-11-09 14:45:18 +0100

    Rspec workbench MyWorkbenchScopes
    Refs #1787

--- lib/stif/my_workbench_scopes.rb
+++ lib/stif/my_workbench_scopes.rb
@@ -1,21 +1,23 @@
-class Stif::MyWorkbenchScopes
-  attr_accessor :workbench
+module Stif
+  class MyWorkbenchScopes
+    attr_accessor :workbench
 
-  def initialize(workbench)
-    @workbench = workbench
-  end
+    def initialize(workbench)
+      @workbench = workbench
+    end
 
-  def line_scope(initial_scope)
-    ids = self.parse_functional_scope
-    ids ? initial_scope.where(objectid: ids) : initial_scope
-  end
+    def line_scope(initial_scope)
+      ids = self.parse_functional_scope
+      ids ? initial_scope.where(objectid: ids) : initial_scope
+    end
 
-  def parse_functional_scope
-    return false unless @workbench.organisation.sso_attributes
-    begin
-      JSON.parse @workbench.organisation.sso_attributes['functional_scope']
-    rescue Exception => e
-      Rails.logger.error "MyWorkbenchScopes : #{e}"
+    def parse_functional_scope
+      return false unless @workbench.organisation.sso_attributes
+      begin
+        JSON.parse @workbench.organisation.sso_attributes['functional_scope']
+      rescue Exception => e
+        Rails.logger.error "MyWorkbenchScopes : #{e}"
+      end
     end
   end
 end
