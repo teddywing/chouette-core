b456753eb511aeb33464c52c1780988a9e6d3091
Xinhui <xinhui.xu@af83.com>
2016-11-15 11:50:54 +0100

    Fix rspec sync message criticity

--- app/models/stop_area_referential_sync_message.rb
+++ app/models/stop_area_referential_sync_message.rb
@@ -1,6 +1,6 @@
 class StopAreaReferentialSyncMessage < ActiveRecord::Base
   belongs_to :stop_area_referential_sync
-  enum criticity: [:info, :warning, :danger]
+  enum criticity: [:info, :warning, :error]
 
   validates :criticity, presence: true
 end
