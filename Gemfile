0c6be980835a9ec2bac980b3c01b0d06544805c5
Alban Peignier <alban.peignier@af83.com>
2016-11-18 19:27:51 +0100

    Complete ReferentialMetadata model and specs. Refs #1782

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
+# coding: utf-8
 source 'http://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/referential_metadata.rb
+++ app/models/referential_metadata.rb
@@ -3,14 +3,16 @@ class ReferentialMetadata < ActiveRecord::Base
   belongs_to :referential_source, class_name: 'Referential'
   has_array_of :lines, class_name: 'Chouette::Line'
 
+  validates :referential, presence: true
+  validates :lines, presence: true
+  validates :periodes, presence: true
+
   scope :include_lines, -> (line_ids) { where('line_ids && ARRAY[?]', line_ids) }
   scope :include_dateranges, -> (dateranges) { where('periodes && ARRAY[?]', dateranges) }
 
   def self.new_from from
-    ReferentialMetadata.new({
-      referential_source: from.referential_source,
-      line_ids: from.line_ids,
-      periodes: from.periodes
-    })
+    from.dup.tap do |metadata|
+      metadata.referential_id = nil
+    end
   end
 end
