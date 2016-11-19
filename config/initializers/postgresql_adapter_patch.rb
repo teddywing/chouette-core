5e24b492252fbfbd266e5266108174b1f1babab5
Alban Peignier <alban.peignier@af83.com>
2016-11-19 19:39:40 +0100

    Unnormalize daterange read from PostgreSQL. Removes exclude_end which makes complex to read human range end. Refs #1782

--- config/initializers/postgresql_adapter_patch.rb
+++ config/initializers/postgresql_adapter_patch.rb
@@ -35,3 +35,20 @@ class ActiveRecord::ConnectionAdapters::PostgreSQLColumn
   end
 
 end
+
+module ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::OID
+  class DateRange < Range
+    # Unnormalize daterange
+    # [2016-11-19,2016-12-26) -> 2016-11-19..2016-12-25
+    def type_cast(value)
+      result = super value
+
+      if result.respond_to?(:exclude_end?) && result.exclude_end?
+        ::Range.new(result.begin, result.end - 1, false)
+      else
+        result
+      end
+    end
+  end
+  register_type 'daterange', DateRange.new(:date)
+end
