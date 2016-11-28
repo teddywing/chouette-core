0414bbe2b5791224b07d3ed86cb7f9189baea4eb
jpl <jean-paul.lescouzeres@af83.com>
2016-11-28 12:04:04 +0100

    Refs #2081: adding labels on itineraries form

--- app/assets/javascripts/es6_browserified/components/Todo.js
+++ app/assets/javascripts/es6_browserified/components/Todo.js
@@ -7,34 +7,37 @@ const Todo = (props) => {
     <div className='list-group-item'>
       <div className='row'>
         <div className='col-lg-5 col-md-5 col-sm-4 col-xs-5'>
-          <div style={{display: 'inline-block', width: '20%', verticalAlign: 'middle', textAlign: 'right'}}>
+          <div style={{display: 'inline-block', width: '20%', verticalAlign: 'middle', textAlign: 'right', marginTop: 24}}>
             <span className='label label-default' style={{marginRight: 10}}>{props.value.stoparea_id}</span>
           </div>
 
           <div style={{display: 'inline-block', width: '80%', verticalAlign: 'middle'}}>
+            <label>Arrêt</label>
             <BSelect2 id={'route_stop_points_' + props.id} value={props.value} onChange={props.onChange} index={props.index} />
           </div>
         </div>
 
         <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2'>
           <div style={{display: 'inline-block', width: '100%', verticalAlign: 'middle'}}>
+            <label>Montée</label>
             <select className='form-control' value={props.value.for_boarding} id="for_boarding" onChange={props.onSelectChange}>
-              <option value="forbidden">interdit</option>
-              <option value="normal">normal</option>
+              <option value="normal">Montée autorisée</option>
+              <option value="forbidden">Montée interdite</option>
             </select>
           </div>
         </div>
         <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2'>
           <div style={{display: 'inline-block', width: '100%', verticalAlign: 'middle'}}>
+            <label>Descente</label>
             <select className='form-control' value={props.value.for_alighting} id="for_alighting" onChange={props.onSelectChange}>
-              <option value="forbidden">interdit</option>
-              <option value="normal">normal</option>
+              <option value="normal">Descente autorisée</option>
+              <option value="forbidden">Descente interdite</option>
             </select>
           </div>
         </div>
 
         <div className='col-lg-3 col-md-3 col-sm-4 col-xs-3' style={{textAlign: 'right'}}>
-          <div className='btn-group btn-group-sm'>
+          <div className='btn-group btn-group-sm' style={{marginTop: 24}}>
             <div
               className={'btn btn-primary' + (props.first ? ' disabled' : '')}
               onClick={props.onMoveUpClick}
