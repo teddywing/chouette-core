e02513c6d12802dde77caf5ea0e1374f870416b7
jpl <jean-paul.lescouzeres@af83.com>
2016-12-19 14:44:45 +0100

    happy x-mas

--- app/assets/javascripts/es6_browserified/journey_patterns/components/JourneyPattern.js
+++ app/assets/javascripts/es6_browserified/journey_patterns/components/JourneyPattern.js
@@ -10,12 +10,17 @@ const JourneyPattern = (props) => {
       <p className='small'><strong>Published name: </strong>{props.value.published_name}</p>
 
       <p className='small'><strong>Stop points: </strong></p>
-      <ul>
+      <ul className='list-group'>
         {props.value.stop_points.map((stopPoint, index) =>
           <li
             key={ index }
+            className='list-group-item clearfix'
           >
-            <span>{stopPoint.id}</span><span>{stopPoint.name}</span><span>{stopPoint.checked.toString()}</span>
+            <span className='label label-default' style={{marginRight: 5}}>{stopPoint.id}</span>
+            <span>{stopPoint.name}</span>
+            <span className='pull-right'>
+              <input type='checkbox' id={stopPoint.id} checked={stopPoint.checked}></input>
+            </span>
           </li>
         )}
       </ul>
