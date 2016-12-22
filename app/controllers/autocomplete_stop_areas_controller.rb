fa218d1d95ca05be0c0b663021a2945ecfe6dc5a
Xinhui <xinhui.xu@af83.com>
2016-12-22 14:56:13 +0100

    Limite route stop_areas to zdep area type
    Refs #2250

--- app/controllers/autocomplete_stop_areas_controller.rb
+++ app/controllers/autocomplete_stop_areas_controller.rb
@@ -14,23 +14,20 @@ class AutocompleteStopAreasController < InheritedResources::Base
   protected
 
   def collection
-    result = []
-    if physical_filter?
-     result = referential.stop_areas.physical
-    elsif target_type? && relation_parent?
-     result = Chouette::StopArea.new( :area_type => params[ :target_type ] ).possible_parents
-    elsif target_type? && relation_children?
-     result = Chouette::StopArea.new( :area_type => params[ :target_type ] ).possible_children
-    else
-      result = referential.stop_areas
+    scope = referential.stop_areas
+    scope = scope.physical if physical_filter?
+    if target_type?
+      scope = scope.where(area_type: params[:target_type])
+      scope = scope.possible_parents if relation_parent?
+      scope = scope.possible_parents if relation_children?
     end
     args = [].tap{|arg| 3.times{arg << "%#{params[:q]}%"}}
-    @stop_areas = result.where("name ILIKE ? OR registration_number ILIKE ? OR objectid ILIKE ?", *args).limit(50)
+    @stop_areas = scope.where("name ILIKE ? OR registration_number ILIKE ? OR objectid ILIKE ?", *args).limit(50)
     @stop_areas
   end
 
   def target_type?
-    params.has_key?( :target_type) && params.has_key?( :relation )
+    params.has_key?( :target_type)
   end
 
   def relation_parent?
