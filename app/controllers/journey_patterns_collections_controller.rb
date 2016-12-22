0c83b3223e458e1af0a469ba5c11bc308f9fe057
Xinhui <xinhui.xu@af83.com>
2016-12-22 12:17:56 +0100

    Wip update journey_patterns collections
    Refs #2213

--- app/controllers/journey_patterns_collections_controller.rb
+++ app/controllers/journey_patterns_collections_controller.rb
@@ -1,6 +1,4 @@
 class JourneyPatternsCollectionsController < ChouetteController
-  defaults :resource_class => Chouette::JourneyPattern
-
   respond_to :html
   respond_to :json
 
@@ -12,14 +10,42 @@ class JourneyPatternsCollectionsController < ChouetteController
   alias_method :route, :parent
 
   def show
+    journey_patterns_state
+  end
+
+  def update
+    state = JSON.parse request.raw_post
+    state.each do |item|
+      journey_pattern = journey_pattern_by_objectid(item['object_id'])
+      journey_pattern_update_stop_points(journey_pattern, item['stop_points']) if journey_pattern
+    end
+
+    journey_patterns_state
+  end
+
+  protected
+
+  def journey_pattern_update_stop_points journey_pattern, stop_points
+    stop_points.each do |sp|
+      stop_id = sp['id']
+      exist   = journey_pattern.stop_area_ids.include?(stop_id)
+      next if exist && sp['checked']
+
+      stop_point = route.stop_points.find_by(stop_area_id: stop_id)
+      if sp['checked'] && !exist
+        journey_pattern.stop_points << stop_point
+      else
+        journey_pattern.stop_points.delete(stop_point)
+      end
+    end
+  end
+
+  def journey_patterns_state
     @q = route.journey_patterns.includes(:stop_points)
     @journey_patterns ||= @q.paginate(:page => params[:page]).order(:name)
   end
 
-  def update
-    ap "-----Call to #{params[:action]} from #{params[:controller]}"
-    ap '------------------------------------------------------------'
-    ap params
-    ap '------------------------------------------------------------'
+  def journey_pattern_by_objectid objectid
+    Chouette::JourneyPattern.find_by(objectid: objectid)
   end
 end
