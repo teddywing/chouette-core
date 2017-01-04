ea69fbeb8bbb981e09b7f68da77fa63902d9e468
Xinhui <xinhui.xu@af83.com>
2017-01-04 11:39:48 +0100

    Restore migration 20161208112130_create_routing_constraint_zones

--- db/migrate/20161208112130_create_routing_constraint_zones.rb
+++ db/migrate/20161208112130_create_routing_constraint_zones.rb
@@ -1,19 +1,11 @@
 class CreateRoutingConstraintZones < ActiveRecord::Migration
-  def up
-    unless table_exists? :routing_constraint_zones
-      create_table :routing_constraint_zones do |t|
-        t.string :name
-        t.integer :stop_area_ids, array: true
-        t.belongs_to :line, index: true
+  def change
+    create_table :routing_constraint_zones do |t|
+      t.string :name
+      t.integer :stop_area_ids, array: true
+      t.belongs_to :line, index: true
 
-        t.timestamps
-      end
-    end
-  end
-
-  def down
-    if table_exists? :routing_constraint_zones
-      drop_table :routing_constraint_zones
+      t.timestamps
     end
   end
 end
