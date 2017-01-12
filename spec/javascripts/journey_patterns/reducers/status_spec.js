da1f31e0515dfe1e910ca0e2b2fb86572e0c7665
jpl <jean-paul.lescouzeres@af83.com>
2017-01-12 10:45:00 +0100

    adding status spec

---
+++ spec/javascripts/journey_patterns/reducers/status_spec.js
@@ -0,0 +1,34 @@
+var statusReducer = require('es6_browserified/journey_patterns/reducers/status')
+
+let state = {}
+
+describe('status reducer', () => {
+  beforeEach(() => {
+    state = {
+      fetchSuccess: false,
+      isFetching: false
+    }
+  })
+
+  it('should return the initial state', () => {
+    expect(
+      statusReducer(undefined, {})
+    ).toEqual({})
+  })
+
+  it('should handle UNAVAILABLE_SERVER', () => {
+    expect(
+      statusReducer(state, {
+        type: 'UNAVAILABLE_SERVER'
+      })
+    ).toEqual(Object.assign({}, state, {fetchSuccess: false}))
+  })
+
+  it('should handle RECEIVE_JOURNEY_PATTERNS', () => {
+    expect(
+      statusReducer(state, {
+        type: 'RECEIVE_JOURNEY_PATTERNS'
+      })
+    ).toEqual(Object.assign({}, state, {fetchSuccess: true}))
+  })
+})
