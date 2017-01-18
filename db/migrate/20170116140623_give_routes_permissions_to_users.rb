cb18311c611395ec361e0fcecc61618b9069bc93
Vlatka Pavisic <vlatka.pavisic@gmail.com>
2017-01-18 11:45:29 +0100

    Refs #2426 : Permissions migration

--- db/migrate/20170116140623_give_routes_permissions_to_users.rb
+++
@@ -1,8 +0,0 @@
-class GiveRoutesPermissionsToUsers < ActiveRecord::Migration
-  def change
-    User.find_each do |user|
-      user.permissions =['routes.create', 'routes.edit', 'routes.destroy']
-      user.save!
-    end
-  end
-end
