999ae8f20e1aefac46108c98aa0ef681d49d344c
Thomas Haddad <thomas.haddad@af83.com>
2017-01-20 11:22:31 +0100

    Refs #2405: Add fetch nearby stopAreas in olMap component and temporarily fix area_type
    Signed-off-by: Thomas Shawarma Haddad <thomas.haddad@af83.com>

--- app/assets/javascripts/es6_browserified/itineraries/components/OlMap.js
+++ app/assets/javascripts/es6_browserified/itineraries/components/OlMap.js
@@ -6,10 +6,42 @@ class OlMap extends Component{
   constructor(props){
     super(props)
   }
-  componentDidUpdate(prev, next) {
-    if(prev.value.olMap.isOpened == false){
+
+  fetchApiForMap(extent, id){
+    const origin = window.location.origin
+    const path = window.location.pathname.split('/', 3).join('/')
+    let params = []
+    for ( var i = 0; i < extent.length; i++ )
+      params.push(encodeURIComponent(i) + '=' + encodeURIComponent(extent[i]))
+    params = params.join("&")
+    const urlJSON = origin + path + "/autocomplete_stop_areas/" + id + "/around?" + params
+
+
+    let req = new Request(urlJSON, {
+      credentials: 'same-origin',
+      method: 'GET',
+      contentType: 'application/json; charset=utf-8',
+      Accept: 'application/json',
+      headers: {
+        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+      }
+    })
+    fetch(req)
+      .then(response => {
+        if(!response.ok) {
+          return false
+        }
+        return response.json()
+      }).then((json) => {
+        console.log(json)
+      })
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevProps.value.olMap.isOpened == false && this.props.value.olMap.isOpened == true){
+      var that = this
       var map = new ol.Map({
-        target: 'stoppoint_map' + this.props.index,
+        target: 'stoppoint_map' + that.props.index,
         layers: [
         new ol.layer.Tile({
           source: new ol.source.OSM()
@@ -24,6 +56,10 @@ class OlMap extends Component{
           zoom: 18
         })
       });
+      let extent = map.getView().calculateExtent(map.getSize())
+      setTimeout(()=>{
+        that.fetchApiForMap(extent, that.props.value.stoparea_id)
+      }, 1000)
       // TODO when fetching, use extent value in EPSG 4326
       // var extent = map.getView().calculateExtent(map.getSize());
     }
