7d975c31a7fc874bb83fa00ff658a50826ff6070
jpl <jean-paul.lescouzeres@af83.com>
2017-02-03 16:25:40 +0100

    Refs #2518: adding search_bar component, on workbench#show

--- app/controllers/workbenches_controller.rb
+++ app/controllers/workbenches_controller.rb
@@ -4,12 +4,15 @@ class WorkbenchesController < BreadcrumbController
   respond_to :html, :only => [:show]
 
   def show
-    @wbench_refs = if params[:show_all]
-      Workbench.find(params[:id]).all_referentials.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 30)
+    if params[:show_all]
+      @q = Workbench.find(params[:id]).all_referentials.ransack(params[:q])
     else
-      Workbench.find(params[:id]).referentials.ready.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 30)
-      # Workbench.find(params[:id]).referentials.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 30)
+      @q = Workbench.find(params[:id]).referentials.ready.ransack(params[:q])
+      # @q = Workbench.find(params[:id]).referentials.ransack(params[:q])
     end
+
+    @collection = @q.result(distinct: true)
+    @wbench_refs = @collection.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 30)
 
     show! do
       build_breadcrumb :show
