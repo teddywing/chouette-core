09176e5bb9a7171d64c43917e201c82163fafda6
jpl <jean-paul.lescouzeres@af83.com>
2017-02-03 10:50:30 +0100

    Refs #2482: adding sortable columns to workbenches#show

--- app/controllers/workbenches_controller.rb
+++ app/controllers/workbenches_controller.rb
@@ -5,14 +5,23 @@ class WorkbenchesController < BreadcrumbController
 
   def show
     @wbench_refs = if params[:show_all]
-      Workbench.find(params[:id]).all_referentials.paginate(page: params[:page], per_page: 20)
+      Workbench.find(params[:id]).all_referentials.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 30)
     else
-      Workbench.find(params[:id]).referentials.ready.paginate(page: params[:page], per_page: 20)
+      Workbench.find(params[:id]).referentials.ready.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 30)
+      # Workbench.find(params[:id]).referentials.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 30)
     end
 
     show! do
       build_breadcrumb :show
     end
+  end
+
+  private
+  def sort_column
+    Workbench.find(params[:id]).referentials.include?(params[:sort]) ? params[:sort] : 'name'
+  end
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : 'asc'
   end
 
 end
