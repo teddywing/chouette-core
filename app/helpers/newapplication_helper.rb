aac370c4966ff6587ce108cb507c12891e8bd59e
jpl <jean-paul.lescouzeres@af83.com>
2017-02-09 14:08:31 +0100

    Refs #2482: updating referentials#show, according to new layout

--- app/helpers/newapplication_helper.rb
+++ app/helpers/newapplication_helper.rb
@@ -201,7 +201,7 @@ module NewapplicationHelper
 
     firstRow = content_tag :div, '', class: 'row' do
       # Left part with pageicon & pagetitle & desc
-      left = content_tag :div, '', class: 'col-lg-9 col-md-8 col-sm-8 col-xs-7' do
+      left = content_tag :div, '', class: 'col-lg-9 col-md-8 col-sm-7 col-xs-7' do
         picon = content_tag :div, '', class: 'page-icon' do
           content_tag :span, '', class: "fa fa-lg fa-#{pageicon}"
         end
@@ -214,7 +214,7 @@ module NewapplicationHelper
         picon + ptitle
       end
       # Right part with meta & mainaction
-      right = content_tag :div, '', class: 'col-lg-3 col-md-4 col-sm-4 col-xs-5 text-right' do
+      right = content_tag :div, '', class: 'col-lg-3 col-md-4 col-sm-5 col-xs-5 text-right' do
         content_tag :div, '', class: 'page-action' do
           a = content_tag :div, meta.html_safe, class: 'small'
           b = mainaction.try(:html_safe)
@@ -229,6 +229,19 @@ module NewapplicationHelper
     content_tag :div, '', class: 'page_header' do
       content_tag :div, '', class: 'container-fluid' do
         firstRow + capture(&block)
+      end
+    end
+  end
+
+  # ModalBox Builder
+  def modalbox id, &block
+    content_tag(:div, '', class: 'modal fade', id: id, tabindex: 1, role: 'dialog') do
+      content_tag(:div, '', class: 'modal-container') do
+        content_tag(:div, '', class: 'modal-dialog') do
+          content_tag(:div, '', class: 'modal-content') do
+            yield
+          end
+        end
       end
     end
   end
