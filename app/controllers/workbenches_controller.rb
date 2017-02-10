cc6744ecf9cf940417fabfaa34b0a857196352ae
jpl <jean-paul.lescouzeres@af83.com>
2017-02-10 14:51:01 +0100

    Refs #2482: fixing :show_all feature according to new specs (feat. Luc Donnet)

--- app/controllers/workbenches_controller.rb
+++ app/controllers/workbenches_controller.rb
@@ -3,9 +3,7 @@ class WorkbenchesController < BreadcrumbController
   respond_to :html, :only => [:show]
 
   def show
-    scope = Workbench.find(params[:id])
-    scope = params[:show_all] ? scope.all_referentials : scope.referentials.ready
-    # scope = scope.referentials
+    scope = Workbench.find(params[:id]).all_referentials
     @q = scope.ransack(params[:q])
 
     @collection = @q.result(distinct: true)
--- spec/features/workbenches_spec.rb
+++ spec/features/workbenches_spec.rb
@@ -10,7 +10,7 @@ describe 'Workbenches', type: :feature do
   let!(:line) { create :line, line_referential: line_referential }
   let!(:referential_metadatas) { Array.new(2) { |i| create :referential_metadata, lines: [line], referential: referentials[i] } }
 
-  let!(:ready_referential) { create(:referential, workbench: workbenches.first, ready: true) }
+  let!(:ready_referential) { create(:referential, workbench: workbenches.first, metadatas: referential_metadatas, ready: true) }
   let!(:unready_referential) { create(:referential, workbench: workbenches.first) }
 
   describe 'show' do
@@ -18,13 +18,6 @@ describe 'Workbenches', type: :feature do
       visit workbench_path(workbenches.first)
       expect(page).to have_content(ready_referential.name)
       expect(page).not_to have_content(unready_referential.name)
-    end
-
-    it 'shows all ready referentials if that option is chosen' do
-      visit workbench_path(workbenches.first)
-      click_link I18n.t('referentials.show.show_all_referentials')
-      expect(page).to have_content(referentials.first.name)
-      expect(page).to have_content(referentials.last.name)
     end
   end
 end
