070c93312ab8db54d6b5e5babbf5ea433ebd6f33
Thomas Haddad <thomas.haddad@af83.com>
2017-02-28 17:41:00 +0100

    Fix fetch API in jp when no entries
    Signed-off-by: Thomas Shawarma Haddad <thomas.haddad@af83.com>
    Signed-off-by: Jean-Paul Lescouzeres <jean-paul.lescouzeres@af83.com>

--- app/assets/javascripts/es6_browserified/journey_patterns/reducers/stopPointsList.js
+++ app/assets/javascripts/es6_browserified/journey_patterns/reducers/stopPointsList.js
@@ -1,12 +1,16 @@
-const stopPointsList = (state = {}, action) => {
+const stopPointsList = (state = [], action) => {
   switch (action.type) {
     case 'RECEIVE_JOURNEY_PATTERNS':
-      let sp = action.json[0].stop_points
-      let spArray = []
-      sp.map((s) => {
-        spArray.push(s.name)
-      })
-      return [...spArray]
+      if(action.json.length != 0){
+        let sp = action.json[0].stop_points
+        let spArray = []
+        sp.map((s) => {
+          spArray.push(s.name)
+        })
+        return [...spArray]
+      }else{
+        return state
+      }
     default:
       return state
   }
