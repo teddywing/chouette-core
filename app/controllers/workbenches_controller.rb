156133d5bdbc05fd283f50396f776b43e49e5249
Xinhui <xinhui.xu@af83.com>
2017-03-06 11:41:36 +0100

    Fix workbench#show filter referential by periode range
    Refs #2665

--- app/controllers/workbenches_controller.rb
+++ app/controllers/workbenches_controller.rb
@@ -5,6 +5,7 @@ class WorkbenchesController < BreadcrumbController
   def show
     scope = Workbench.find(params[:id])
     scope = params[:q] ? scope.all_referentials : scope.referentials.ready
+    scope = scope.in_periode(ransack_periode) if ransack_periode
     @q = scope.ransack(params[:q])
     @q.organisation_name_eq_any ||= current_organisation.name unless params[:q]
 
@@ -27,8 +28,17 @@ class WorkbenchesController < BreadcrumbController
   def sort_column
     Workbench.find(params[:id]).referentials.include?(params[:sort]) ? params[:sort] : 'name'
   end
+
   def sort_direction
     %w[asc desc].include?(params[:direction]) ?  params[:direction] : 'asc'
   end
 
+  def ransack_periode
+    return unless params[:q] && params[:q]['validity_period']
+
+    periode     = params[:q]['validity_period']
+    start_range = Date.civil(periode["begin_gteq(1i)"].to_i, periode["begin_gteq(2i)"].to_i, periode["begin_gteq(3i)"].to_i)
+    end_range   = Date.civil(periode["end_lteq(1i)"].to_i, periode["end_lteq(2i)"].to_i, periode["end_lteq(3i)"].to_i)
+    start_range..end_range
+  end
 end
