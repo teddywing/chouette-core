11815cdde26915348982b6b2e5161fadb71ed65d
jpl <jean-paul.lescouzeres@af83.com>
2017-03-09 14:51:03 +0100

    Refs #2728: fixing duplicate_vj modal layout

--- app/assets/javascripts/es6_browserified/vehicle_journeys/components/tools/DuplicateVehicleJourney.js
+++ app/assets/javascripts/es6_browserified/vehicle_journeys/components/tools/DuplicateVehicleJourney.js
@@ -48,7 +48,7 @@ class DuplicateVehicleJourney extends Component {
                     <form>
                       <div className='modal-body'>
                         <div className='row'>
-                          <div className='col-lg-10 col-md-9 col-sm-8 col-xs-12'>
+                          <div className='col-lg-8 col-md-8 col-sm-8 col-xs-12'>
                             <div className='form-group'>
                               <label className='control-label is-required'>Nombre de courses à créer et dupliquer</label>
                               <input
@@ -62,7 +62,7 @@ class DuplicateVehicleJourney extends Component {
                                 />
                             </div>
                           </div>
-                          <div className='col-lg-2 col-md-3 col-sm-4 col-xs-12'>
+                          <div className='col-lg-4 col-md-4 col-sm-4 col-xs-12'>
                             <div className='form-group'>
                               <label className='control-label is-required'>Avec un décalage de</label>
                               <input
