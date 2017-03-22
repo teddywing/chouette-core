612043979188da4e0343efbc4606a232941b7423
Alban Peignier <alban.peignier@af83.com>
2017-03-22 12:06:59 +0100

    Fixes test in RemoveStopPointsRouteFk. Refs #2864

--- db/migrate/20170317114614_remove_stop_points_route_fk.rb
+++ db/migrate/20170317114614_remove_stop_points_route_fk.rb
@@ -1,6 +1,6 @@
 class RemoveStopPointsRouteFk < ActiveRecord::Migration
   def change
-    if foreign_keys(:stop_points).include?(:stoppoint_route_fkey)
+    if foreign_keys(:stop_points).any? { |f| f.options[:name] = :stoppoint_route_fkey }
       remove_foreign_key :stop_points, name: :stoppoint_route_fkey
     end
   end
