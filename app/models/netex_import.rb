eaf643b91f724a3f3b31855299b3682619cf818c
Luc Donnet <luc.donnet@free.fr>
2017-03-31 17:56:13 +0200

    Delete json suffix on IEV call

--- app/models/netex_import.rb
+++ app/models/netex_import.rb
@@ -3,9 +3,9 @@ class NetexImport < Import
   after_create :launch_java_import
 
   def launch_java_import
-    logger.warn  "Call iev get #{Rails.configuration.iev_url}/boiv_iev/referentials/importer/new.json?id=#{id}"
+    logger.warn  "Call iev get #{Rails.configuration.iev_url}/boiv_iev/referentials/importer/new?id=#{id}"
     begin
-      Net::HTTP.get(Rails.configuration.iev_url, "/boiv_iev/referentials/importer/new.json?id=#{id}")
+      Net::HTTP.get(Rails.configuration.iev_url, "/boiv_iev/referentials/importer/new?id=#{id}")
     rescue
       logger.error("IEV server error")
     end
