bc09c8f16726f520c116e158ef3390dc84736bd2
jpl <jean-paul.lescouzeres@af83.com>
2017-04-13 16:07:09 +0200

    Refs #2982: starting with reactux integration

--- app/assets/javascripts/es6_browserified/time_tables/components/Metas.js
+++ app/assets/javascripts/es6_browserified/time_tables/components/Metas.js
@@ -11,27 +11,47 @@ const reorderArray = (arr) =>{
 const Metas = ({day_types}) => {
   day_types = reorderArray(day_types)
   return (
-    <div>
-      <h2>Metas</h2>
-        <div className="form-group labelled-checkbox-group">
-          {day_types.map((day, i) =>
-            <div className="lcbx-group-item"
-              key={i}
-            >
-              <div className="checkbox">
-                <label>
-                  <input
-                    onChange={(e) => {e.preventDefault()}}
-                    id={i}
-                    type="checkbox"
-                    checked={day ? 'checked' : ''}
-                  />
-                  <span className='lcbx-group-item-label'>{weekDays[i]}</span>
-                </label>
+    <div className='form-horizontal'>
+      <div className="row">
+        <div className="col-lg-12">
+          {/* comment (name) */}
+          <div className="form-group"></div>
+
+          {/* color */}
+          <div className="form-group"></div>
+
+          {/* tags */}
+          <div className="form-group"></div>
+
+          {/* day_types */}
+          <div className="form-group">
+            <label htmlFor="" className="control-label col-sm-4">
+              Journées d'applications pour les périodes ci-dessous
+            </label>
+            <div className="col-sm-8">
+              <div className="form-group labelled-checkbox-group">
+                {day_types.map((day, i) =>
+                  <div className="lcbx-group-item"
+                    key={i}
+                    >
+                    <div className="checkbox">
+                      <label>
+                        <input
+                          onChange={(e) => {e.preventDefault()}}
+                          id={i}
+                          type="checkbox"
+                          checked={day ? 'checked' : ''}
+                          />
+                        <span className='lcbx-group-item-label'>{weekDays[i]}</span>
+                      </label>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
-          )}
+          </div>
         </div>
+      </div>
     </div>
   )
 }
