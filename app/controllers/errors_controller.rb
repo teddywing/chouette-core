23403a7344eeb2d0c4f052c984baa49b8c58154c
Vlatka Pavisic <vlatka.pavisic@gmail.com>
2017-04-20 13:48:21 +0200

    Refs #3033 : Attempt to display 403 page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,13 @@
 class ErrorsController < ApplicationController
   def not_found
-    render :status => 404, :formats => [:html]
+    render status: 404, formats: [:html]
   end
 
   def server_error
-    render :status => 500, :formats => [:html]
+    render status: 500, formats: [:html]
+  end
+
+  def not_allowed
+    render status: 403, formats: [:html]
   end
 end
