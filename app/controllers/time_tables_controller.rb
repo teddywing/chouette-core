489a05ee6469b711140ca6d4ad6ebe296bc2d82f
Xinhui <xinhui.xu@af83.com>
2017-04-26 15:10:35 +0200

    Wip TimeTables#update save day_types
    Refs #2899

--- app/controllers/time_tables_controller.rb
+++ app/controllers/time_tables_controller.rb
@@ -58,7 +58,6 @@ class TimeTablesController < ChouetteController
 
   def update
     state  = JSON.parse request.raw_post
-
     respond_to do |format|
       format.json { render json: state, status: state['errors'] ? :unprocessable_entity : :ok }
     end
