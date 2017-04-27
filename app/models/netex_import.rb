b30e59f75a67a609764b0a8cd63c35ce55cfe584
Luc Donnet <luc.donnet@free.fr>
2017-04-27 15:55:01 +0200

    Fix netex import for strange behaviour of Net::HTTP.get

--- app/models/netex_import.rb
+++ app/models/netex_import.rb
@@ -5,9 +5,10 @@ class NetexImport < Import
   def launch_java_import
     logger.warn  "Call iev get #{Rails.configuration.iev_url}/boiv_iev/referentials/importer/new?id=#{id}"
     begin
-      Net::HTTP.get(Rails.configuration.iev_url, "/boiv_iev/referentials/importer/new?id=#{id}")
-    rescue
-      logger.error("IEV server error")
+      Net::HTTP.get(URI("#{Rails.configuration.iev_url}/boiv_iev/referentials/importer/new?id=#{id}"))
+    rescue Exception => e
+      logger.error "IEV server error : e.message"
+      logger.error e.backtrace.inspect
     end
   end
 end
