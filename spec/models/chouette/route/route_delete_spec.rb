407885e41987c0c64913194396c5667483416192
Robert <robert.dober@af83.com>
2017-05-04 18:15:46 +0200

    Refs # 3297; behavior speced

---
+++ spec/models/chouette/route/route_delete_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe Chouette::Route, :type => :model do
+
+  subject { create(:route) }
+
+
+  context "delete a route" do
+    let( :vehicle_journey ){ create :vehicle_journey }
+
+    it "deletes the associated journey_patterns" do
+      expected_delta = subject.journey_patterns.count
+      expect( expected_delta ).to be_positive
+      expect{ subject.delete }.to change{Chouette::JourneyPattern.count}.by -expected_delta
+    end
+
+    it "deletes the associated stop_points" do
+      expected_delta = subject.stop_points.count
+      expect( expected_delta ).to be_positive
+      expect{ subject.delete }.to change{Chouette::StopPoint.count}.by -expected_delta
+    end
+
+    it "does not delete the associated stop_areas" do
+      count = subject.stop_points.count
+      expect( count ).to be_positive
+      expect{ subject.delete }.not_to change{Chouette::StopArea.count}
+    end
+
+    it "deletes the associated vehicle_journeys" do
+      expect{ vehicle_journey.route.delete }.to change{Chouette::VehicleJourney.count}.by -1
+    end
+
+    it "does not delete the corresponding time_tables" do
+      tt = create :time_table
+      tt.vehicle_journeys << vehicle_journey
+      tables = vehicle_journey.route.time_tables
+      expect( tables ).not_to be_empty
+      expect{ vehicle_journey.route.delete }.not_to change{Chouette::TimeTable.count}
+    end
+
+    it "does not delete the associated line" do
+      expect( subject.line ).not_to be_nil
+      expect{ subject.delete }.not_to change{Chouette::Line.count}
+    end
+  end
+end
