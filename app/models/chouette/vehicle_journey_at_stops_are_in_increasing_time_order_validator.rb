8435122816197160a3fdab89b1f178266ed2d653
Teddy Wing <teddy.wing@af83.com>
2017-05-23 17:15:22 +0200

    IncreasingTimeOrderValidator: Improve whitespace
    Make this more readable by realigning the code vertically.
    
    Refs #870

--- app/models/chouette/vehicle_journey_at_stops_are_in_increasing_time_order_validator.rb
+++ app/models/chouette/vehicle_journey_at_stops_are_in_increasing_time_order_validator.rb
@@ -8,11 +8,21 @@ module Chouette
     def increasing_times(vehicle_journey)
       # TODO: Rename `previous`
       previous = nil
+
       # TODO: Move `select` to a new named method
-      vehicle_journey.vehicle_journey_at_stops.select{|vjas| vjas.departure_time && vjas.arrival_time}.each do |vjas|
-        vehicle_journey.errors.add( :vehicle_journey_at_stops, 'time gap overflow') unless vjas.increasing_times_validate( previous)
-        previous = vjas
-      end
+      vehicle_journey
+        .vehicle_journey_at_stops
+        .select { |vjas| vjas.departure_time && vjas.arrival_time }
+        .each do |vjas|
+          unless vjas.increasing_times_validate(previous)
+            vehicle_journey.errors.add(
+              :vehicle_journey_at_stops,
+              'time gap overflow'
+            )
+          end
+
+          previous = vjas
+        end
     end
   end
 end
