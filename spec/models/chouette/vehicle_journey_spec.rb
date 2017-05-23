3082bcfa0b0b0b46f5a1c2470ba90f9dc140f644
Teddy Wing <teddy.wing@af83.com>
2017-05-23 15:21:32 +0200

    VehicleJourney: Move `#increasing_times` validator to a new class
    Create a new validator class for this validation. By splitting the
    increasing time validation into a separate group, we can unify the
    functionality that's currently split between `VehicleJourney` and
    `VehicleJourneyAtStop`. This will allow us to go to a single place to
    look for the validation.
    
    Move the `#increasing_times` method into our new class with minor
    modification to make it work. Move the test for the method into a new
    spec file.
    
    Refs #870

--- spec/models/chouette/vehicle_journey_spec.rb
+++ spec/models/chouette/vehicle_journey_spec.rb
@@ -466,18 +466,6 @@ describe Chouette::VehicleJourney, :type => :model do
   end
 
   context "when following departure times exceeds gap" do
-    describe "#increasing_times" do
-      before(:each) do
-        subject.vehicle_journey_at_stops[0].departure_time = subject.vehicle_journey_at_stops[1].departure_time - 5.hour
-        subject.vehicle_journey_at_stops[0].arrival_time = subject.vehicle_journey_at_stops[0].departure_time
-        subject.vehicle_journey_at_stops[1].arrival_time = subject.vehicle_journey_at_stops[1].departure_time
-      end
-      it "should make instance invalid" do
-        subject.increasing_times
-        expect(subject.vehicle_journey_at_stops[1].errors[:departure_time]).not_to be_blank
-        expect(subject).not_to be_valid
-      end
-    end
     describe "#update_attributes" do
       let!(:params){ {"vehicle_journey_at_stops_attributes" => {
             "0"=>{"id" => subject.vehicle_journey_at_stops[0].id ,"arrival_time" => 1.minutes.ago,"departure_time" => 1.minutes.ago},
