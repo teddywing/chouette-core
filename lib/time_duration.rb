bbfdc698cdaaa92ca6842fb566a9ace17ddd558a
Teddy Wing <teddy.wing@af83.com>
2017-05-24 12:04:02 +0200

    TimeDuration: Add a method comment above `.exceeds_gap?`
    Describe what's going on here to hopefully make it easier to understand.
    
    Refs #870

--- lib/time_duration.rb
+++ lib/time_duration.rb
@@ -1,4 +1,13 @@
 module TimeDuration
+  # `earlier` and `later` are times. Get the duration between those times and
+  # check whether it's longer than the given `duration`.
+  #
+  # Example:
+  #   TimeDuration.exceeds_gap?(
+  #     4.hours,
+  #     Time.now,
+  #     Time.now + 2.hours
+  #   )
   def self.exceeds_gap?(duration, earlier, later)
     duration < (later - earlier)
   end
