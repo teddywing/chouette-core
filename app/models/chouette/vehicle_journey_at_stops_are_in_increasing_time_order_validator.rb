32aeba90d7c79505162a3aaf39094e3b2bb682d1
Teddy Wing <teddy.wing@af83.com>
2017-05-24 11:18:58 +0200

    IncreasingTimeOrderValidator: Rename `previous` to `previous_at_stop`
    Be more explicit about what this value is by putting it in the name.
    
    Refs #870

--- app/models/chouette/vehicle_journey_at_stops_are_in_increasing_time_order_validator.rb
+++ app/models/chouette/vehicle_journey_at_stops_are_in_increasing_time_order_validator.rb
@@ -6,33 +6,32 @@ module Chouette
     end
 
     def increasing_times(vehicle_journey)
-      # TODO: Rename `previous`
-      previous = nil
+      previous_at_stop = nil
 
       vehicle_journey
         .vehicle_journey_at_stops
         .select { |vjas| vjas.departure_time && vjas.arrival_time }
         .each do |vjas|
-          unless vjas.increasing_times_validate(previous)
+          unless vjas.increasing_times_validate(previous_at_stop)
             vehicle_journey.errors.add(
               :vehicle_journey_at_stops,
               'time gap overflow'
             )
           end
 
-          previous = vjas
+          previous_at_stop = vjas
         end
     end
 
-    def increasing_times_validate( previous)
+    def increasing_times_validate(previous_at_stop)
       result = true
-      return result unless previous
+      return result unless previous_at_stop
 
-      if exceeds_gap?( previous.departure_time, departure_time)
+      if exceeds_gap?(previous_at_stop.departure_time, departure_time)
         result = false
         errors.add( :departure_time, 'departure time gap overflow')
       end
-      if exceeds_gap?( previous.arrival_time, arrival_time)
+      if exceeds_gap?(previous_at_stop.arrival_time, arrival_time)
         result = false
         errors.add( :arrival_time, 'arrival time gap overflow')
       end
