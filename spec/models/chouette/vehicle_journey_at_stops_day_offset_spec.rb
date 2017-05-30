6f7ad36f3b958a719a8f39c75749a641ace189c5
Teddy Wing <teddy.wing@af83.com>
2017-05-30 15:41:16 +0200

    vehicle_journey_at_stops_day_offset_spec.rb: Fill in other 2 test cases
    Add the test code for the other two cases we had originally thought of.
    Here we test that the offset is correct when both `arrival_time` &
    `departure_time` are on the same day, and then that the day offset will
    get incremented correctly when there are multiple days represented.
    
    In order for multiple days to work, there need to be intermediary
    at-stops that occur on the offsetted day.
    
    Refs #3596

--- spec/models/chouette/vehicle_journey_at_stops_day_offset_spec.rb
+++ spec/models/chouette/vehicle_journey_at_stops_day_offset_spec.rb
@@ -38,9 +38,67 @@ describe Chouette::VehicleJourneyAtStop do
     end
 
     it "increments day offset when an at_stop passes midnight the next day" do
+      at_stops = []
+      [
+        ['22:30', '22:35'],
+        ['01:02', '01:14'],
+      ].each do |arrival_time, departure_time|
+        at_stops << build(
+          :vehicle_journey_at_stop,
+          arrival_time: arrival_time,
+          departure_time: departure_time,
+
+          # TODO: make this the default
+          arrival_day_offset: 0,
+          departure_day_offset: 0
+        )
+      end
+
+      offsetter = Chouette::VehicleJourneyAtStopsDayOffset.new(at_stops)
+
+      offsetter.calculate!
+
+      expect(at_stops[0].arrival_day_offset).to eq(0)
+      expect(at_stops[0].departure_day_offset).to eq(0)
+
+      expect(at_stops[1].arrival_day_offset).to eq(1)
+      expect(at_stops[1].departure_day_offset).to eq(1)
     end
 
     it "increments day offset for multi-day offsets" do
+      at_stops = []
+      [
+        ['22:30', '22:35'],
+        ['01:02', '01:14'],
+        ['04:30', '04:35'],
+        ['00:00', '00:04'],
+      ].each do |arrival_time, departure_time|
+        at_stops << build(
+          :vehicle_journey_at_stop,
+          arrival_time: arrival_time,
+          departure_time: departure_time,
+
+          # TODO: make this the default
+          arrival_day_offset: 0,
+          departure_day_offset: 0
+        )
+      end
+
+      offsetter = Chouette::VehicleJourneyAtStopsDayOffset.new(at_stops)
+
+      offsetter.calculate!
+
+      expect(at_stops[0].arrival_day_offset).to eq(0)
+      expect(at_stops[0].departure_day_offset).to eq(0)
+
+      expect(at_stops[1].arrival_day_offset).to eq(1)
+      expect(at_stops[1].departure_day_offset).to eq(1)
+
+      expect(at_stops[2].arrival_day_offset).to eq(1)
+      expect(at_stops[2].departure_day_offset).to eq(1)
+
+      expect(at_stops[3].arrival_day_offset).to eq(2)
+      expect(at_stops[3].departure_day_offset).to eq(2)
     end
   end
 end
