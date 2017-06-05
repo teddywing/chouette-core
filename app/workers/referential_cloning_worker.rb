c59f237817100f73baec56e04bfaf172d7d59fc1
Robert <robert.dober@af83.com>
2017-06-05 17:23:12 +0200

    Refs: #3604; Bad schema renaming and removal fixed

--- app/workers/referential_cloning_worker.rb
+++ app/workers/referential_cloning_worker.rb
@@ -6,7 +6,7 @@ class ReferentialCloningWorker
     ref_cloning = ReferentialCloning.find id
 
     source_schema = ref_cloning.source_referential.slug
-    target_schema = "#{source_schema}_tmp"
+    target_schema = ref_cloning.target_referential.slug
 
     clone_schema ref_cloning, source_schema, target_schema
   end
@@ -16,11 +16,7 @@ class ReferentialCloningWorker
   def clone_schema ref_cloning, source_schema, target_schema
     ref_cloning.run!
 
-  require 'pry'
-  binding.pry
     StoredProcedures.invoke_stored_procedure(:clone_schema, source_schema, target_schema, true) 
-    execute_sql "DROP SCHEMA #{source_schema} CASCADE;"
-    execute_sql "ALTER SCHEMA #{target_schema} RENAME TO #{source_schema};"
 
     ref_cloning.successful!
   rescue Exception => e
