3fb578fb9ab68041d630db6294320fb7065a2966
Robert <robert.dober@af83.com>
2017-06-16 16:59:33 +0200

    hotfix pry

--- app/controllers/devise/cas_sessions_controller.rb
+++ app/controllers/devise/cas_sessions_controller.rb
@@ -20,8 +20,6 @@ class Devise::CasSessionsController < Devise::SessionsController
     if LoginPolicy.new(current_user).boiv?
       redirect_to after_sign_in_path_for(current_user)
     else
-      require 'pry'
-      binding.pry
       redirect_to root_path, flash: {alert: t('devise.sessions.new.unauthorized')}
     end
   end
