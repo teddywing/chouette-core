533c1a55df630080cd0ff449e229717ba7ace212
Teddy Wing <teddy.wing@af83.com>
2017-06-19 18:08:28 +0200

    Revert "factories/workbenches: Add a new `:with_referential` trait"
    This reverts commit 073ac0eb3330787532f551d20abe4600a0615446.
    
    I no longer need this factory. Originally added it for a test where I
    thought I needed a collection of `Workbench`es. Turns out I instead
    needed a collection of `Referential`s. Thus we don't have a need of
    creating a workbench with a referential together.

--- spec/factories/workbenches.rb
+++ spec/factories/workbenches.rb
@@ -5,24 +5,5 @@ FactoryGirl.define do
     association :organisation, :factory => :organisation
     association :line_referential
     association :stop_area_referential
-
-    trait :with_referential do
-      # TODO: change all => to :
-      # association :referential,
-      #   organisation: { organisation }
-
-      # after(:stub) do |workbench, evaluator|
-      #   
-      # end
-
-      referentials do |workbench|
-        [association(
-          :referential,
-          organisation: workbench.organisation,
-          line_referential: workbench.line_referential,
-          stop_area_referential: workbench.stop_area_referential
-        )]
-      end
-    end
   end
 end
