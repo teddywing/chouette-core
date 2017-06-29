8a1104733347858f5081d360d71ce1041f6e5a60
jpl <jean-paul.lescouzeres@af83.com>
2017-06-29 12:30:07 +0200

    Refs #3776: adding first-step error system to JP

--- app/assets/javascripts/es6_browserified/journey_patterns/components/JourneyPattern.js
+++ app/assets/javascripts/es6_browserified/journey_patterns/components/JourneyPattern.js
@@ -43,75 +43,79 @@ class JourneyPattern extends Component{
     )
   }
 
+  getErrors(errors) {
+    let err = Object.keys(errors).map((key, index) => {
+      return (
+        <li key={index} style={{listStyleType: 'disc'}}>
+          <strong>{key}</strong> { errors[key] }
+        </li>
+      )
+    })
+
+    return (
+      <ul className="alert alert-danger">{err}</ul>
+    )
+  }
+
   render() {
     this.previousCity = undefined
 
     return (
-      <div className={'t2e-item' + (this.props.value.deletable ? ' disabled' : '') + (this.props.value.object_id ? '' : ' to_record')}>
+      <div className={'t2e-item' + (this.props.value.deletable ? ' disabled' : '') + (this.props.value.object_id ? '' : ' to_record') + (this.props.value.errors ? ' has-error': '')}>
         {/* Errors */}
-        {(this.props.value.errors) && (
-          <ul className='alert alert-danger small' style={{paddingLeft: 30}}>
-            {Object.keys(this.props.value.errors).map(function(key, i) {
-              return (
-                <li key={i} style={{listStyleType: 'disc'}}>
-                  <strong>'{key}'</strong> {this.props.value.errors[key]}
-                  </li>
-                )
-              })}
-            </ul>
-          )}
+        {/* this.props.value.errors ? this.getErrors(this.props.value.errors) : '' */}
 
-          <div className='th'>
-            <div className='strong mb-xs'>{this.props.value.object_id ? actions.humanOID(this.props.value.object_id) : '-'}</div>
-            <div>{this.props.value.registration_number}</div>
-            <div>{actions.getChecked(this.props.value.stop_points).length} arrêt(s)</div>
+        <div className='th'>
+          <div className='strong mb-xs'>{this.props.value.object_id ? actions.humanOID(this.props.value.object_id) : '-'}</div>
+          <div>{this.props.value.registration_number}</div>
+          <div>{actions.getChecked(this.props.value.stop_points).length} arrêt(s)</div>
 
-            <div className={this.props.value.deletable ? 'btn-group disabled' : 'btn-group'}>
-              <div
-                className={this.props.value.deletable ? 'btn dropdown-toggle disabled' : 'btn dropdown-toggle'}
-                data-toggle='dropdown'
-                >
-                <span className='fa fa-cog'></span>
-              </div>
-              <ul className='dropdown-menu'>
-                <li className={(this.props.value.deletable || this.props.status.policy['journey_patterns.edit'] == false) ? 'disabled' : ''}>
-                  <button
-                    type='button'
-                    onClick={this.props.onOpenEditModal}
-                    data-toggle='modal'
-                    data-target='#JourneyPatternModal'
-                    >
-                    Editer
+          <div className={this.props.value.deletable ? 'btn-group disabled' : 'btn-group'}>
+            <div
+              className={this.props.value.deletable ? 'btn dropdown-toggle disabled' : 'btn dropdown-toggle'}
+              data-toggle='dropdown'
+              >
+              <span className='fa fa-cog'></span>
+            </div>
+            <ul className='dropdown-menu'>
+              <li className={(this.props.value.deletable || this.props.status.policy['journey_patterns.edit'] == false) ? 'disabled' : ''}>
+                <button
+                  type='button'
+                  onClick={this.props.onOpenEditModal}
+                  data-toggle='modal'
+                  data-target='#JourneyPatternModal'
+                  >
+                  Editer
+                </button>
+              </li>
+              <li className={this.props.value.object_id ? '' : 'disabled'}>
+                {this.vehicleJourneyURL(this.props.value.object_id)}
+              </li>
+              <li className={'delete-action' + ((this.props.status.policy['journey_patterns.edit'] == false)? ' disabled' : '')}>
+                <button
+                  type='button'
+                  disabled={(this.props.status.policy['journey_patterns.edit'] == false)? 'disabled' : ''}
+                  onClick={(e) => {
+                    e.preventDefault()
+                    this.props.onDeleteJourneyPattern(this.props.index)}
+                  }
+                  >
+                    <span className='fa fa-trash'></span>Supprimer
                   </button>
                 </li>
-                <li className={this.props.value.object_id ? '' : 'disabled'}>
-                  {this.vehicleJourneyURL(this.props.value.object_id)}
-                </li>
-                <li className={'delete-action' + ((this.props.status.policy['journey_patterns.edit'] == false)? ' disabled' : '')}>
-                  <button
-                    type='button'
-                    disabled={(this.props.status.policy['journey_patterns.edit'] == false)? 'disabled' : ''}
-                    onClick={(e) => {
-                      e.preventDefault()
-                      this.props.onDeleteJourneyPattern(this.props.index)}
-                    }
-                    >
-                      <span className='fa fa-trash'></span>Supprimer
-                    </button>
-                  </li>
-                </ul>
-              </div>
+              </ul>
             </div>
-
-            {this.props.value.stop_points.map((stopPoint, i) =>{
-              return (
-                <div key={i} className='td'>
-                  {this.cityNameChecker(stopPoint)}
-                </div>
-              )
-            })}
           </div>
-        )
+
+          {this.props.value.stop_points.map((stopPoint, i) =>{
+            return (
+              <div key={i} className='td'>
+                {this.cityNameChecker(stopPoint)}
+              </div>
+            )
+          })}
+        </div>
+      )
   }
 }
 
