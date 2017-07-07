7c7451dc80b6f9b6092b3262fe524a8252309b1a
Robert <robert.dober@af83.com>
2017-07-07 18:29:38 +0200

    Fixes: #3478@2.5h
    - Decorator Spec Setup `spec/support/decortor_helpers.rb`
    - Speced
    - Fixed

---
+++ spec/decorators/referential_decorator_spec.rb
@@ -0,0 +1,76 @@
+RSpec.describe ReferentialDecorator, type: [:helper, :decorator] do
+
+  let( :object ){ build_stubbed :referential }
+  let( :referential ){ object }
+  let( :user ){ build_stubbed :user }
+
+  describe 'delegation' do
+    it 'delegates all' do
+      %i{xx xxx anything save!}.each do |method|
+        expect( object ).to receive(method)
+      end
+      # Almost as powerful as Quicktest :P
+      %i{xx xxx anything save!}.each do |method|
+        subject.send method
+      end
+    end
+  end
+
+  describe 'action links for' do
+
+    context 'unarchived referential' do
+      context 'no rights' do
+        it 'has only a Calendar action' do
+          expect_action_link_hrefs.to eq([referential_time_tables_path(object)])
+        end
+      end
+
+      context 'all rights and different organisation' do 
+
+        let( :user ){ build_stubbed :allmighty_user }
+
+        it 'has only default actions' do
+          expect_action_link_elements.to be_empty
+          expect_action_link_hrefs.to eq([
+            referential_time_tables_path(object),
+          ])
+        end
+      end
+      context 'all rights and same organisation' do 
+
+        let( :user ){ build_stubbed :allmighty_user, organisation: referential.organisation }
+
+        it 'has all actions' do
+          expect_action_link_elements.to eq(%w{Purger})
+          expect_action_link_hrefs.to eq([
+            referential_time_tables_path(object),
+            new_referential_path(from: object),
+            archive_referential_path(object),
+            referential_path(object)
+          ])
+        end
+      end
+    end
+
+    context 'archived referential' do
+      before { referential.archived_at = 42.seconds.ago }
+      context 'no rights' do
+        it 'has only a Calendar action' do
+          expect_action_link_hrefs.to eq([referential_time_tables_path(object)])
+        end
+      end
+
+      context 'all rights and different organisation' do
+        let( :user ){ build_stubbed :allmighty_user }
+        it 'has only default actions' do
+          expect_action_link_elements.to be_empty
+          expect_action_link_hrefs.to eq([
+            referential_time_tables_path(object),
+          ])
+        end
+      end
+    end
+  end
+
+
+end
---
+++ spec/support/decorator_helpers.rb
@@ -0,0 +1,27 @@
+module Support
+
+  module DecoratorHelpers
+    def self.included(into)
+      into.instance_eval do
+        subject{ object.decorate }
+        let( :policy ){ ::Pundit.policy(user_context, object) }
+        let( :user_context ){ UserContext.new(user, referential: referential) }
+
+        before do
+          allow_any_instance_of(Draper::HelperProxy).to receive(:policy).and_return policy
+        end
+      end
+    end
+
+    def expect_action_link_hrefs
+      expect( subject.action_links.select(&Link.method(:===)).map(&:href) )
+    end
+    def expect_action_link_elements
+      expect( subject.action_links.select(&HTMLElement.method(:===)).map(&:content) )
+    end
+  end
+end
+
+RSpec.configure do | c |
+  c.include Support::DecoratorHelpers, type: :decorator
+end
