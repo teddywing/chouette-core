faa9bce7ab2c4f8c2d09200387f70c2ed1d2322a
Robert <robert.dober@af83.com>
2017-07-07 16:38:23 +0200

    User Permissions Computation tested in controllers vehicle_journeys and journey_patterns

--- spec/controllers/referentials_controller_spec.rb
+++ spec/controllers/referentials_controller_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe ReferentialsController, :type => :controller do
 
   login_user
---
+++ spec/controllers/vehicle_journeys_controller_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe VehicleJourneysController, :type => :controller do
+
+  before do
+    @user = build_stubbed(:allmighty_user)
+  end
+
+  describe 'user_permissions' do
+    let( :referential ){ build_stubbed(:referential) }
+    let( :user_context ){ UserContext.new(@user, referential: referential) }
+
+    before do
+      allow(controller).to receive(:pundit_user).and_return(user_context)
+    end
+
+    it 'computes them correctly if not authorized' do
+      expect( controller.send(:user_permissions) ).to eq({'vehicle_journeys.create'  => false,
+                                                   'vehicle_journeys.destroy' => false,
+                                                   'vehicle_journeys.update'  => false }.to_json)
+    end
+    it 'computes them correctly if authorized' do
+      @user.organisation_id = referential.organisation_id
+      expect( controller.send(:user_permissions) ).to eq({'vehicle_journeys.create'  => true,
+                                                   'vehicle_journeys.destroy' => true,
+                                                   'vehicle_journeys.update'  => true }.to_json)
+    end
+  end
+
+end
