c7ceb38801b853154cdae31672ff2c105c8b191f
Teddy Wing <teddy.wing@af83.com>
2017-07-07 18:25:48 +0200

    Remove ReferentialLines#index
    Delete this route and associated template files. Wasn't sure if using
    `exclude:` in the routes file was the best way to do this, as I'm not
    sure if there are other routes we want to exclude from here also.
    
    I visited the page because I'm migrating templates to the new table
    builder helper and this one uses table builder. Upon visiting the page,
    it looked pretty wonky, and after consulting with Jean-Paul and Luc, it
    turns out this page isn't used any more because the table of
    `Chouette::Line`s rendered there is now instead displayed on the
    Referentials#show page, obviating the need for this page.
    
    Refs #3479

--- app/controllers/referential_lines_controller.rb
+++ app/controllers/referential_lines_controller.rb
@@ -10,18 +10,6 @@ class ReferentialLinesController < ChouetteController
 
   belongs_to :referential
 
-  def index
-    @hide_group_of_line = referential.group_of_lines.empty?
-    index! do |format|
-      format.html {
-        if collection.out_of_bounds?
-          redirect_to params.merge(:page => 1)
-        end
-        build_breadcrumb :index
-      }
-    end
-  end
-
   def show
     @routes = resource.routes
 
