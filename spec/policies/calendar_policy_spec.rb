1ab9c3364ffcd777dae39bd9154dca031cc00db0
Robert <robert.dober@af83.com>
2017-07-10 21:06:07 +0200

    Refs: #4021@0.15h; Fixes Policy Tests

--- spec/policies/calendar_policy_spec.rb
+++ spec/policies/calendar_policy_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CalendarPolicy, type: :policy do
 
 
   permissions :create? do
-    it_behaves_like 'permitted policy and same organisation', 'calendars.create', archived: true
+    it_behaves_like 'permitted policy', 'calendars.create', archived: true
   end
   permissions :destroy? do
     it_behaves_like 'permitted policy and same organisation', 'calendars.destroy', archived: true
@@ -14,7 +14,7 @@ RSpec.describe CalendarPolicy, type: :policy do
     it_behaves_like 'permitted policy and same organisation', 'calendars.update', archived: true
   end
   permissions :new? do
-    it_behaves_like 'permitted policy and same organisation', 'calendars.create', archived: true
+    it_behaves_like 'permitted policy', 'calendars.create', archived: true
   end
   permissions :update? do
     it_behaves_like 'permitted policy and same organisation', 'calendars.update', archived: true
