c9bf8f63221699d4b7c97d5f3bd4cced917884d8
jpl <jean-paul.lescouzeres@af83.com>
2017-07-11 12:54:46 +0200

    Refs #2646: fix route view test

--- spec/views/routes/show.html.erb_spec.rb
+++ spec/views/routes/show.html.erb_spec.rb
@@ -1,42 +1,34 @@
 require 'spec_helper'
 
-describe "/routes/show", :type => :view do
+describe "/routes/show", type: 'view' do
 
-  # Fix tests to permit params to be used...
+  assign_referential
+  let!(:line) { assign :line, create(:line) }
+  let!(:route) { assign :route, create(:route, :line => line).decorate(context: {referential: referential, line: line }) }
+  let!(:route_sp) { assign :route_sp, route.stop_points }
 
-  # assign_referential
-  # let!(:line) { assign :line, create(:line) }
-  # let!(:route) { assign :route, create(:route, :line => line) }
-  # let!(:route_sp) { assign :route_sp, route.stop_points }
-  # let!(:map) { assign(:map, double(:to_html => '<div id="map"/>'.html_safe)) }
-  #
-  #
-  # before do
-  #   self.params.merge!({
-  #     id: route.id,
-  #     line_id: line.id,
-  #     referential_id: referential.id
-  #   })
-  # end
+  before do
+    self.params.merge!({
+      id: route.id,
+      line_id: line.id,
+      referential_id: referential.id
+    })
+    allow(view).to receive(:current_referential).and_return(referential)
+  end
 
-  # it "should render h1 with the route name" do
-  #   render
-  #   expect(rendered).to have_selector("h1", :text => Regexp.new(line.name))
-  # end
+  it "should render h1 with the route name" do
+    render
+    expect(rendered).to have_selector("h1", :text => Regexp.new(route.name))
+  end
 
-  # it "should display a map with class 'line'" do
-  #   render
-  #   expect(rendered).to have_selector("#map", :class => 'line')
-  # end
+  it "should render a link to edit the route" do
+    render
+    expect(rendered).to have_selector("a[href='#{view.edit_referential_line_route_path(referential, line, route)}']")
+  end
 
-  # it "should render a link to edit the route" do
-  #   render
-  #   expect(rendered).to have_selector("a[href='#{view.edit_referential_line_route_path(referential, line, route)}']")
-  # end
-  #
-  # it "should render a link to remove the route" do
-  #   render
-  #   expect(rendered).to have_selector("a[href='#{view.referential_line_route_path(referential, line, route)}']")
-  # end
+  it "should render a link to remove the route" do
+    render
+    expect(rendered).to have_selector("a[href='#{view.referential_line_route_path(referential, line, route)}']")
+  end
 
 end
