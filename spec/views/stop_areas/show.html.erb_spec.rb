7ea4ab54b53e9d14bf4562df24ed341fbe297f9e
Teddy Wing <teddy.wing@af83.com>
2017-07-12 12:01:04 +0200

    stop_areas/show.html.erb_spec.rb: Decorate stop area
    In order to get the `#action_links` method to be found by the new table
    builder, decorate the stop area.
    
    Refs #3479

--- spec/views/stop_areas/show.html.erb_spec.rb
+++ spec/views/stop_areas/show.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "/stop_areas/show", :type => :view do
 
   let!(:stop_area_referential) { assign :stop_area_referential, stop_area.stop_area_referential }
-  let!(:stop_area) { assign :stop_area, create(:stop_area) }
+  let!(:stop_area) { assign :stop_area, create(:stop_area).decorate }
   let!(:access_points) { assign :access_points, [] }
   let!(:map) { assign(:map, double(:to_html => '<div id="map"/>'.html_safe)) }
 
