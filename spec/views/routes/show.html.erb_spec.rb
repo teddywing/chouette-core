25e0a8c896d2562a91f3fcea8e5f6ffba972f149
Teddy Wing <teddy.wing@af83.com>
2017-07-12 11:50:08 +0200

    routes/show.html.erb_spec.rb: Update for new table builder helper
    Need to decorate the stop points collection in order to be able to call
    `#action_links` on the objects therein.
    
    Added a stub for `pundit_user` to get around
    `TableBuilderHelper#table_builder_2` having a dependency on
    `ApplicationController#pundit_user`. Don't like this stub at all. Should
    be doing this in a better way.
    
    Refs #3479

--- spec/views/routes/show.html.erb_spec.rb
+++ spec/views/routes/show.html.erb_spec.rb
@@ -3,7 +3,12 @@ RSpec.describe "/routes/show", type: :view do
   assign_referential
   let!(:line) { assign :line, create(:line) }
   let!(:route) { assign :route, create(:route, :line => line).decorate(context: {referential: referential, line: line }) }
-  let!(:route_sp) { assign :route_sp, route.stop_points }
+  let!(:route_sp) do
+    assign :route_sp, ModelDecorator.decorate(
+      route.stop_points,
+      with: StopPointDecorator
+    )
+  end
 
   before do
     self.params.merge!({
@@ -12,6 +17,10 @@ RSpec.describe "/routes/show", type: :view do
       referential_id: referential.id
     })
     allow(view).to receive(:current_referential).and_return(referential)
+    allow(view).to receive(:pundit_user).and_return(UserContext.new(
+      build_stubbed(:user),
+      referential
+    ))
   end
 
   it "should render h1 with the route name" do
