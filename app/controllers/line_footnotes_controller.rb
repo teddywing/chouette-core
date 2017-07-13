383ccd7d1337276c5d086e3dc077c7235fe0541d
Luc Donnet <luc.donnet@free.fr>
2017-07-13 16:44:46 +0200

    Update policies to make a custom behaviour on footnotes Refs #4094 @1

--- app/controllers/line_footnotes_controller.rb
+++ app/controllers/line_footnotes_controller.rb
@@ -2,6 +2,9 @@ class LineFootnotesController < ChouetteController
   defaults resource_class: Chouette::Line, collection_name: 'lines', instance_name: 'line'
   belongs_to :referential
 
+  before_action :authorize_resource, only: [:destroy_footnote, :edit_footnote, :show_footnote, :update_footnote]
+  before_action :authorize_resource_class, only: [:create_footnote, :index_footnote, :new_footnote]
+
   def edit
     edit! do
       build_breadcrumb :edit
@@ -17,12 +20,16 @@ class LineFootnotesController < ChouetteController
 
   protected
 
-  alias_method :line, :resource
-
-  # overrides default
-  def check_policy
+  protected
+  def authorize_resource
     authorize resource, "#{action_name}_footnote?".to_sym
   end
+
+  def authorize_resource_class
+    authorize resource_class, "#{action_name}_footnote?".to_sym
+  end
+
+  alias_method :line, :resource
 
   def resource
     @line ||= current_referential.lines.find params[:line_id]
