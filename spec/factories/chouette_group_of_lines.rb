78bbc9a39f2012bf6d066c3a0f603473fdd340b3
Xinhui <xinhui.xu@af83.com>
2017-07-26 16:20:58 +0200

    Fix objectid in group of line spec

--- spec/factories/chouette_group_of_lines.rb
+++ spec/factories/chouette_group_of_lines.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory  :group_of_line, :class => Chouette::GroupOfLine do
     sequence(:name) { |n| "Group Of Line #{n}" }
-    sequence(:objectid) { |n| "chouette:test:GroupOfLine:#{n}" }
+    sequence(:objectid) { |n| "test:GroupOfLine:#{n}:loc" }
     sequence(:registration_number) { |n| "#{n}" }
 
     association :line_referential
