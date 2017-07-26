33b4fe1f39501ca36b7a32421e9e1fea93f89e12
Xinhui <xinhui.xu@af83.com>
2017-07-26 16:16:19 +0200

    Fix provider_id & default_values on empty string objectid

--- app/models/concerns/stif_netex_attributes_support.rb
+++ app/models/concerns/stif_netex_attributes_support.rb
@@ -8,8 +8,18 @@ module StifNetexAttributesSupport
     validates_numericality_of :object_version
     validate :objectid_format_compliance
 
-    before_validation :default_values, :on => :create
+    before_validation :default_values, on: :create
     # before_save :increment_workbench_local_id
+  end
+
+  module ClassMethods
+    def object_id_key
+      model_name
+    end
+
+    def model_name
+      ActiveModel::Name.new self, Chouette, self.name.demodulize
+    end
   end
 
   def objectid
@@ -17,16 +27,15 @@ module StifNetexAttributesSupport
   end
 
   def provider_id
-    self.referential.workbench.organisation
+    self.referential.workbench.organisation.name.parameterize
   end
 
   def object_type
-    self.class
+    self.model_name
   end
 
-
   def local_id
-    ''
+    'LOCAL_ID'
   end
 
   def boiv_id
@@ -38,7 +47,9 @@ module StifNetexAttributesSupport
   end
 
   def default_values
-    self.object_id ||= Chouette::StifNetexObjectId.create(provider_id, object_type, local_id, boiv_id)
+    if self.objectid.to_s.empty?
+      self.objectid = Chouette::StifNetexObjectid.create(provider_id, object_type, local_id, boiv_id)
+    end
     self.object_version ||= 1
   end
 
