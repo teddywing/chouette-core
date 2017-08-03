05bb43f370f30785d4b3da99cfe7cafadd8810a2
Robert <robert.dober@af83.com>
2017-08-03 13:02:17 +0200

    after migration'

--- db/schema.rb
+++ db/schema.rb
@@ -284,12 +284,12 @@ ActiveRecord::Schema.define(version: 20170802141224) do
     t.datetime "started_at"
     t.datetime "ended_at"
     t.string   "token_download"
-    t.string   "type"
+    t.string   "type",                  limit: 255
     t.integer  "parent_id",             limit: 8
     t.string   "parent_type"
+    t.integer  "current_step",                      default: 0
+    t.integer  "total_steps",                       default: 0
     t.datetime "notified_parent_at"
-    t.integer  "current_step",                    default: 0
-    t.integer  "total_steps",                     default: 0
   end
 
   add_index "imports", ["referential_id"], name: "index_imports_on_referential_id", using: :btree
@@ -607,7 +607,7 @@ ActiveRecord::Schema.define(version: 20170802141224) do
 
   create_table "stop_areas", id: :bigserial, force: :cascade do |t|
     t.integer  "parent_id",                       limit: 8
-    t.string   "objectid",                                                            null: false
+    t.string   "objectid",                                                              null: false
     t.integer  "object_version",                  limit: 8
     t.string   "creator_id"
     t.string   "name"
@@ -616,8 +616,8 @@ ActiveRecord::Schema.define(version: 20170802141224) do
     t.string   "registration_number"
     t.string   "nearest_topic_name"
     t.integer  "fare_code"
-    t.decimal  "longitude",                                 precision: 19, scale: 16
-    t.decimal  "latitude",                                  precision: 19, scale: 16
+    t.decimal  "longitude",                                   precision: 19, scale: 16
+    t.decimal  "latitude",                                    precision: 19, scale: 16
     t.string   "long_lat_type"
     t.string   "country_code"
     t.string   "street_name"
@@ -635,7 +635,7 @@ ActiveRecord::Schema.define(version: 20170802141224) do
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "stif_type"
+    t.string   "stif_type",                       limit: 255
   end
 
   add_index "stop_areas", ["name"], name: "index_stop_areas_on_name", using: :btree
@@ -702,18 +702,18 @@ ActiveRecord::Schema.define(version: 20170802141224) do
   add_index "time_table_periods", ["time_table_id"], name: "index_time_table_periods_on_time_table_id", using: :btree
 
   create_table "time_tables", id: :bigserial, force: :cascade do |t|
-    t.string   "objectid",                              null: false
-    t.integer  "object_version",  limit: 8, default: 1
+    t.string   "objectid",                                null: false
+    t.integer  "object_version",  limit: 8,   default: 1
     t.string   "creator_id"
     t.string   "version"
     t.string   "comment"
-    t.integer  "int_day_types",             default: 0
+    t.integer  "int_day_types",               default: 0
     t.date     "start_date"
     t.date     "end_date"
     t.integer  "calendar_id",     limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "color"
+    t.string   "color",           limit: 255
     t.integer  "created_from_id"
   end
 
