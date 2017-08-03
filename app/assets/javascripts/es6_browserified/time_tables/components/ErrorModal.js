a24fd7121f2029e222648c9e45dfabea1ec64ecb
cedricnjanga <ced.njg@gmail.com>
2017-08-03 12:35:32 +0200

    Add traslations for timetable edit

--- app/assets/javascripts/es6_browserified/time_tables/components/ErrorModal.js
+++ app/assets/javascripts/es6_browserified/time_tables/components/ErrorModal.js
@@ -2,21 +2,17 @@ var React = require('react')
 var Component = require('react').Component
 var PropTypes = require('react').PropTypes
 
-const withoutPeriodsWithDaysTypes = "Un calendrier d'application ne peut pas avoir de journée(s) d'application sans période(s)."
-const withPeriodsWithoutDayTypes = "Un calendrier d'application ne peut pas avoir de période(s) sans journée(s) d'application."
-
-
 const ErrorModal = ({dispatch, modal, onModalClose}) => (
   <div className={ 'modal fade ' + ((modal.type == 'error') ? 'in' : '') } id='ErrorModal'>
     <div className='modal-container'>
       <div className='modal-dialog'>
         <div className='modal-content'>
           <div className='modal-header'>
-            <h4 className='modal-title'>Erreur</h4>
+            <h4 className='modal-title'>{window.I18n.fr.time_tables.edit.error_modal.title}</h4>
           </div>
           <div className='modal-body'>
             <div className='mt-md mb-md'>
-              <p>{(modal.modalProps.error == "withoutPeriodsWithDaysTypes") ? withoutPeriodsWithDaysTypes : withPeriodsWithoutDayTypes}</p>
+              <p>{(modal.modalProps.error == "withoutPeriodsWithDaysTypes") ? window.I18n.fr.time_tables.edit.error_modal.withoutPeriodsWithDaysTypes : window.I18n.fr.time_tables.edit.error_modal.withPeriodsWithoutDayTypes}</p>
             </div>
           </div>
           <div className='modal-footer'>
