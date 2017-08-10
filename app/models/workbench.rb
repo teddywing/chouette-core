73e72981b2db451a82614acd91136b8bc53926de
Xinhui <xinhui.xu@af83.com>
2017-08-10 10:42:09 +0200

    add has_many workbench_object_identifiers for workbench

--- app/models/workbench.rb
+++ app/models/workbench.rb
@@ -9,6 +9,8 @@ class Workbench < ActiveRecord::Base
   has_many :group_of_lines, through: :line_referential
   has_many :stop_areas, through: :stop_area_referential
   has_many :imports
+  has_many :workbench_object_identifiers
+
   validates :name, presence: true
   validates :organisation, presence: true
 
