2df1767902996d43b56aa215bea15a033237b3ec
Xinhui <xinhui.xu@af83.com>
2017-08-22 15:33:43 +0200

    Rspec api iboo controller

--- spec/controllers/api/v1/iboo_controller_spec.rb
+++ spec/controllers/api/v1/iboo_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::IbooController, type: :controller do
+  context '#authenticate' do
+    include_context 'iboo authenticated api user'
 
+    it 'should set current organisation' do
+      controller.send(:authenticate)
+      expect(assigns(:current_organisation)).to eq api_key.organisation
+    end
+  end
 end
