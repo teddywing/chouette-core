92484fa903dd7cbf4a40bf6b0b65ed79306c6ae9
Teddy Wing <teddy.wing@af83.com>
2017-08-25 11:24:52 +0200

    ReferentialMetadataKludge: Get line IDs by `objectid`
    Instead of hard-coding an ID for the line IDs, get them by their
    `objectid` field.
    
    Worked on this with Alban.
    
    Even though the Kludge class is for testing purposes only and isn't
    meant to be real, let's take out the hard-coded values.
    
    The `objectid`s correspond to those in the OFFRE_... zip in
    `spec/fixtures/`.

--- lib/referential_metadata_kludge.rb
+++ lib/referential_metadata_kludge.rb
@@ -1,9 +1,15 @@
 module ReferentialMetadataKludge extend self
   
   def make_metadata_from_name!( name, referential_id: )
+    line_ids = Chouette::Line.where(
+      objectid: ['C00108', 'C00109'].map do |id|
+        "STIF:CODIFLIGNE:Line:#{id}"
+      end
+    ).pluck(:id)
+
     ReferentialMetadata.create!(
       referential_id: referential_id,
-      line_ids: [107, 108],
+      line_ids: line_ids,
       periodes: name_to_periods(name))
   end
 
