e19c13975c84cf3cc7d54ab687bfa788a7dcd2bd
Teddy Wing <teddy.wing@af83.com>
2017-08-31 11:41:06 +0200

    WorkbenchImportWorker spec: Reinstate tests
    Don't skip these any more. Working to figure out how to make new specs
    that work with the new code.

--- spec/workers/workbench_import_worker_spec.rb
+++ spec/workers/workbench_import_worker_spec.rb
@@ -1,8 +1,4 @@
-RSpec.describe WorkbenchImportWorker,
-  type: [:worker, :request],
-  skip: "ZipService has been refactored and RetryService was removed. These
-    tests need to be changed to reflect the new state of the code. Skipping
-    them because imports need to be ready for QA testing within a day." do
+RSpec.describe WorkbenchImportWorker, type: [:worker, :request] do
 
   let( :worker ) { described_class.new }
   let( :import ){ build_stubbed :import, token_download: download_token, file: zip_file }
