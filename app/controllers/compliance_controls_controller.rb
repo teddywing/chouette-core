60ad22fa8db3c1f2cbfe532a73deca4b97232a25
Xinhui <xinhui.xu@af83.com>
2017-09-14 16:09:11 +0200

    ComplianceControl Controller
    Refs #4467

---
+++ app/controllers/compliance_controls_controller.rb
@@ -0,0 +1,22 @@
+class ComplianceControlsController < BreadcrumbController
+  defaults resource_class: ComplianceControl
+  belongs_to :compliance_control_set
+
+  def create
+    create!(notice: t('notice.compliance_control.created'))
+  end
+
+  def update
+    path = compliance_control_set_compliance_control_path(parent, resource)
+    update!(notice: t('notice.compliance_control.updated')) { path }
+  end
+
+  def destroy
+    destroy!(notice: t('notice.compliance_control.destroyed'))
+  end
+
+  private
+  def compliance_control_params
+    params.require(:compliance_control).permit(:name, :code, :criticity, :comment, :control_attributes)
+  end
+end
--- app/models/compliance_control.rb
+++ app/models/compliance_control.rb
@@ -3,6 +3,7 @@ class ComplianceControl < ActiveRecord::Base
   belongs_to :compliance_control_block
 
   enum criticity: [:info, :warning, :error]
+
   validates :criticity, presence: true
   validates :name, presence: true
   validates :code, presence: true
