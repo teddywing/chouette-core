e7cd3271cda450f335b4fe259827112191944527
cedricnjanga <ced.njg@gmail.com>
2017-09-19 12:52:42 +0200

    Add compliance control models and some translations

---
+++ app/models/route_control/duplicates.rb
@@ -0,0 +1,14 @@
+module RouteControl
+  class Duplicates < ComplianceControl
+
+    @@default_criticity = :warning
+    @@default_code = "3-Route-4"
+
+    after_initialize do
+      self.name = self.class.name
+      self.code = @@default_code
+      self.criticity = @@default_criticity
+    end
+
+  end
+end
