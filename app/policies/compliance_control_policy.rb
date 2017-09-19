87134a30e859ab024c887c3ef60f79ba1c15c53e
Xinhui <xinhui.xu@af83.com>
2017-09-19 13:58:03 +0200

    Compliance control policy
    Refs #4543

--- app/policies/compliance_control_policy.rb
+++ app/policies/compliance_control_policy.rb
@@ -4,4 +4,16 @@ class ComplianceControlPolicy < ApplicationPolicy
       scope
     end
   end
+
+  def destroy?
+    organisation_match? && user.has_permission?('compliance_controls.destroy')
+  end
+
+  def create?
+    user.has_permission?('compliance_controls.create')
+  end
+
+  def update?
+    organisation_match? && user.has_permission?('compliance_controls.update')
+  end
 end
