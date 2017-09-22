beced2b5817da0082aa9f3af76581a524535d801
Teddy Wing <teddy.wing@af83.com>
2017-09-22 18:58:54 +0200

    Add `has_many` association between `Referential` and `ReferentialSuite`
    `ReferentialSuite` has many `Referential`s. Need to add a foreign key
    column to the `referentials` table to allow that to work.
    
    Refs #3519

--- spec/models/referential_suite_spec.rb
+++ spec/models/referential_suite_spec.rb
@@ -1,4 +1,5 @@
 RSpec.describe ReferentialSuite, type: :model do
   it { should belong_to(:new).class_name('Referential') }
   it { should belong_to(:current).class_name('Referential') }
+  it { should have_many(:referentials) }
 end
