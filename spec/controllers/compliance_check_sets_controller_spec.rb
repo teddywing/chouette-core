efce9eac3812c005409ce87028877e4a40724949
Guillaume <guillaume.perot@af83.com>
2017-09-26 16:29:20 +0200

    show compliance_control_set in compliance_check_set table, fix compliance_check_set spec

--- spec/controllers/compliance_check_sets_controller_spec.rb
+++ spec/controllers/compliance_check_sets_controller_spec.rb
@@ -5,18 +5,16 @@ RSpec.describe ComplianceCheckSetsController, type: :controller do
 
   let(:compliance_check_set) { create :compliance_check_set }
 
-  workbench_compliance_check_sets_path(current_offer_workbench)
-
   describe "GET show" do
     it 'should be successful' do
-      get :show, id: compliance_check_set.id
+      get :show, workbench_id: compliance_check_set.workbench.id, id: compliance_check_set.id
       expect(response).to be_success
     end
   end
 
   describe "GET index" do
     it 'should be successful' do
-      get :index, id: compliance_check_set.id
+      get :index, workbench_id: compliance_check_set.workbench.id, id: compliance_check_set.id
       expect(response).to be_success
     end
   end
