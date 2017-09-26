69080dc24855236c0ef8a981ede60120d90478ba
Robert <robert.dober@af83.com>
2017-09-26 16:40:01 +0200

    Refs: 3521@2.5h; DB setup for view test
            - Manual creation without FG association, might change if modelling can be simplified

--- spec/features/companies_spec.rb
+++ spec/features/companies_spec.rb
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-require 'spec_helper'
-
 describe "Companies", :type => :feature do
   login_user
 
--- spec/features/referential_suites_spec.rb
+++ spec/features/referential_suites_spec.rb
@@ -3,7 +3,10 @@ RSpec.describe 'ReferentialSuites', type: :feature do
   login_user
 
   let(:workbench) { create :workbench }
-  let!(:referential_suite) { create :referential_suite }
+  let(:referential_suite) { workbench.output }
+  let!(:referentials) do
+    3.times.map { |_| create :referential, workbench: workbench, referential_suite: referential_suite }
+  end
 
   describe 'index' do
     before(:each) { visit workbench_output_path(workbench) }
@@ -12,8 +15,14 @@ RSpec.describe 'ReferentialSuites', type: :feature do
       # Breadcrumb
       expect( page ).to have_text('Accueil')
       expect( page ).to have_text("Gestion de l'offre")
+
       # Title
       expect( page ).to have_text('Offres de mon organisation')
+    end
+
+    it 'filtering with Ransack' do
+      fill_in 'q[name_or_objectid_cont]', with: referential_suite.name
+      
     end
 
   end
