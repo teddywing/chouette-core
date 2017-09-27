c3f80c537e28dfe316ee0dcbee2bde2b5307fd79
Xinhui <xinhui.xu@af83.com>
2017-09-27 11:59:44 +0200

    Support of dynamic_attributes in form
    Refs #4467

--- app/models/generic_attribute_min_max.rb
+++ app/models/generic_attribute_min_max.rb
@@ -1,23 +1,23 @@
 class GenericAttributeMinMax < ComplianceControl
-
-
   hstore_accessor :control_attributes, minimum: :integer, maximum: :integer
-  #attribute :minimum, type: :integer, optionnal: true
-  #attribute :maximum, type: :integer, optionnal: true
-  #attribute :target, type: ModelAttribute
 
   @@default_criticity = :warning
   @@default_code = "3-Generic-2"
 
   validate :min_max_values
+
   def min_max_values
     true
   end
 
-  after_initialize do
-    self.name = 'GenericAttributeMinMax'
-    self.code = @@default_code
-    self.criticity = @@default_criticity
+  def self.dynamic_attributes
+    self.hstore_metadata_for_control_attributes.keys
   end
+
+  # after_initialize do
+  #   self.name = 'GenericAttributeMinMax'
+  #   self.code = @@default_code
+  #   self.criticity = @@default_criticity
+  # end
 
 end
