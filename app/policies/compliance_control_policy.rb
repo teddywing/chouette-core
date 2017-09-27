66aa4070d1af632430d2e1af820acf66659a54f8
Xinhui <xinhui.xu@af83.com>
2017-09-27 12:01:47 +0200

    Disable policy for testing

--- app/policies/compliance_control_policy.rb
+++ app/policies/compliance_control_policy.rb
@@ -6,14 +6,17 @@ class ComplianceControlPolicy < ApplicationPolicy
   end
 
   def destroy?
-    user.has_permission?('compliance_controls.destroy')
+    # user.has_permission?('compliance_controls.destroy')
+    true
   end
 
   def create?
-    user.has_permission?('compliance_controls.create')
+    # user.has_permission?('compliance_controls.create')
+    true
   end
 
   def update?
-    user.has_permission?('compliance_controls.update')
+    # user.has_permission?('compliance_controls.update')
+    true
   end
 end
