180e79d33e26dc4fd2cf8737a267c1a6a4e32320
cedricnjanga <ced.njg@gmail.com>
2017-09-28 18:02:32 +0200

    Refs #4626 Add spec to validate presence of compliance_check_set for ComplianceCheckResource

--- app/models/compliance_check_resource.rb
+++ app/models/compliance_check_resource.rb
@@ -2,4 +2,6 @@ class ComplianceCheckResource < ActiveRecord::Base
   extend Enumerize
 
   enumerize :status, in: %w[new successful warning failed]
+
+  validates_presence_of :compliance_check_set
 end
