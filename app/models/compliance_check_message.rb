b55255e7ed06528f3610c43fbfafe10e741bbdcf
Luc Donnet <luc.donnet@af83.com>
2017-09-29 14:01:54 +0200

    Fix enumerize in compliance_check_message Refs #4391

--- app/models/compliance_check_message.rb
+++ app/models/compliance_check_message.rb
@@ -1,4 +1,8 @@
 class ComplianceCheckMessage < ActiveRecord::Base
+  extend Enumerize
+
   belongs_to :compliance_check
   belongs_to :compliance_check_resource
+
+  enumerize :status, in: %i(OK ERROR WARNING IGNORED), scope: true
 end
