4764d2cdc34e44a0040115b99e8984aea5a6b569
cedricnjanga <ced.njg@gmail.com>
2017-09-29 15:32:12 +0200

    Add transport_submode attribute to ComplianceControlBlock + validation on transport mode presence

--- app/models/compliance_control_block.rb
+++ app/models/compliance_control_block.rb
@@ -2,9 +2,17 @@ class ComplianceControlBlock < ActiveRecord::Base
   belongs_to :compliance_control_set
   has_many :compliance_controls, dependent: :destroy
 
-  hstore_accessor :condition_attributes, transport_mode: :string
+  hstore_accessor :condition_attributes, 
+    transport_mode: :string, 
+    transport_sub_mode: :string
+
+    validates_presence_of :transport_mode
 
   def self.transport_modes
-    ["all"] + StifTransportModeEnumerations.transport_mode.values
+    StifTransportModeEnumerations.transport_mode.options
+  end
+
+  def self.transport_sub_modes
+    StifTransportSubmodeEnumerations.transport_submode.options
   end
 end
