04618f6079aabca0d8e5464bc8c1701c15a593fa
cedricnjanga <ced.njg@gmail.com>
2017-09-29 15:35:27 +0200

    Refs #4517 Add module to deal with transport mode and submode

---
+++ app/models/concerns/stif_transport_mode_enumerations.rb
@@ -0,0 +1,6 @@
+module StifTransportModeEnumerations
+  extend ActiveSupport::Concern
+  extend Enumerize
+
+  enumerize :transport_mode, in: %w(air bus coach ferry metro rail trolleyBus tram water cableway funicular other)
+end
