95d63430c5deba078749396bcb919c49b71d1bd7
cedricnjanga <ced.njg@gmail.com>
2017-09-29 15:30:23 +0200

    Refeacto ComplianceControlBlock controller

--- app/controllers/compliance_control_blocks_controller.rb
+++ app/controllers/compliance_control_blocks_controller.rb
@@ -1,41 +1,17 @@
 class ComplianceControlBlocksController < BreadcrumbController
   defaults resource_class: ComplianceControlBlock
   belongs_to :compliance_control_set
+  actions :all, :except => [:show, :index]
 
   def new
     @compliance_control_set = parent
     @compliance_control_block = ComplianceControlBlock.new(compliance_control_set: @compliance_control_set)
   end
 
-  def create
-    create! do |success, failure|
-      success.html { redirect_to compliance_control_set_path(@compliance_control_set) }  
-      failure.html { render :action => :new }
-    end
-  end
-
-  def update
-    update! do |success, failure|
-      success.html { redirect_to compliance_control_set_path(@compliance_control_set) }  
-      failure.html { render :action => :edit }
-    end 
-  end
-
-  def destroy
-    destroy! do |format|
-      format.html { redirect_to compliance_control_set_path(@compliance_control_set) }  
-    end 
-  end
-
   private
 
-  def create_resource compliance_control_block
-    compliance_control_block.transport_mode = params[:compliance_control_block][:transport_mode]
-    super
-  end
-
   def compliance_control_block_params
-    params.require(:compliance_control_block).permit(:transport_mode)
+    params.require(:compliance_control_block).permit(:transport_mode, :transport_sub_mode)
   end
 
 end
