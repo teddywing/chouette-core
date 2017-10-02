88567b111cf172e7b4368ffc02cb1e5cf27b4231
Robert <robert.dober@af83.com>
2017-10-02 22:01:00 +0200

    Refs: #4627@0.2h; Fixes regression for renamed compliance_control factory

---
+++ spec/factories/compliance_controls.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :compliance_control do
+    sequence(:name) { |n| "Compliance control #{n}" }
+    type "GenericAttributeControl::MinMax"
+    criticity :warning
+    code "code"
+    origin_code "code"
+    comment "Text"
+    association :compliance_control_set
+    association :compliance_control_block
+  end
+end
--- spec/factories/compliance_controls/generic_factories.rb
+++ spec/factories/compliance_controls/generic_factories.rb
@@ -1,16 +1,5 @@
 
 FactoryGirl.define do
-  factory :generic_attribute_control_compliance_control do
-    sequence(:name) { |n| "Compliance control #{n}" }
-    type "GenericAttributeControl::MinMax"
-    criticity :warning
-    code "code"
-    origin_code "code"
-    comment "Text"
-    association :compliance_control_set
-    association :compliance_control_block
-  end
-
   factory :generic_attribute_control_min_max, class: 'GenericAttributeControl::MinMax' do
     sequence(:name) { |n| "MinMax control #{n}" }
     association :compliance_control_set
