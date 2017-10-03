983f5953d6df7166e91f52ba5eccaf27b8841f76
cedricnjanga <ced.njg@gmail.com>
2017-10-03 15:32:26 +0200

    Refs #4661 Remove string column from ComplianceCheckSet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003130549) do
+ActiveRecord::Schema.define(version: 20171003133042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -195,7 +195,6 @@ ActiveRecord::Schema.define(version: 20171003130549) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "current_step_id"
-    t.string   "string"
     t.float    "current_step_progress"
     t.string   "name"
     t.datetime "started_at"
