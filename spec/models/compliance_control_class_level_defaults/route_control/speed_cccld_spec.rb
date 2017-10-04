458b841fbeed0fee9a1f6db981c3b18f47a84905
Luc Donnet <luc.donnet@af83.com>
2017-10-04 13:29:01 +0200

    Rename spec test for compliance_control Refs #4690

--- spec/models/compliance_control_class_level_defaults/route_control/speed_cccld_spec.rb
+++
@@ -1,7 +0,0 @@
-
-RSpec.describe RouteControl::Speed, type: :model do
-  let( :default_code ){ "3-VehicleJourney-2" }
-  let( :factory ){ :route_control_speed }
-
-  it_behaves_like 'ComplianceControl Class Level Defaults' 
-end
