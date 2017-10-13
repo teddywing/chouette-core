c266ee913353d67a3674ed6188c2ff96b44407c1
Robert <robert.dober@af83.com>
2017-10-13 16:48:46 +0200

    Refs: #4270@0.6h;
      GenericControlAttribute validation speced for
    
        - MinMax
        - Pattern

---
+++ spec/models/compliance_control_validations/genric_attribute_validation/pattern_validation_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe GenericAttributeControl::Pattern do
+
+  let( :factory ){ :generic_attribute_control_pattern }
+  subject{ build factory }
+
+  context "is valid" do
+    if 'the pattern contains a basic regex' do
+      subject.pattern = 'hel+o?'
+      expect_it.to be_valid
+    end
+  end
+
+  context "is invalid" do
+    it 'if no pattern has been provided' do
+      expect_it.not_to be_valid
+    end
+    it 'if the pattern is empty' do
+      subject.pattern = '  '
+      expect_it.not_to be_valid
+    end
+
+  end
+end
