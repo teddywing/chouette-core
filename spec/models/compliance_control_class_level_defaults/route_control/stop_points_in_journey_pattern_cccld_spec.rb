35619ee35a0694d2693a2aa88cb6e7d40f5678f1
cedricnjanga <ced.njg@gmail.com>
2017-10-16 15:58:17 +0200

    Change default code to toute control / stop point in JP

--- spec/models/compliance_control_class_level_defaults/route_control/stop_points_in_journey_pattern_cccld_spec.rb
+++ spec/models/compliance_control_class_level_defaults/route_control/stop_points_in_journey_pattern_cccld_spec.rb
@@ -1,6 +1,6 @@
 
 RSpec.describe RouteControl::StopPointsInJourneyPattern, type: :model do
-  let( :default_code ){ "3-Route-6" }
+  let( :default_code ){ "3-Route-8" }
   let( :factory ){ :route_control_stop_points_in_journey_pattern }
 
   it_behaves_like 'ComplianceControl Class Level Defaults' 
