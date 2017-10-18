9c0f538419b66d4c2b34312b7c047f1d5041ad3c
Xinhui <xinhui.xu@af83.com>
2017-10-18 15:02:07 +0200

    Fix spec validation failed on contriance control sti subclasses

--- app/models/route_control/minimum_length.rb
+++ app/models/route_control/minimum_length.rb
@@ -1,6 +1,6 @@
 module RouteControl
   class MinimumLength < ComplianceControl
-    enumerize :criticity, in: %i(error), scope: true
+    enumerize :criticity, in: %i(error), scope: true, default: :error
 
     def self.default_code; "3-Route-6" end
   end
