beaa8e127e6b6104cb377c9a2be4a6e4ecae6da9
Robert <robert.dober@af83.com>
2017-10-21 10:34:16 +0200

    Added meta tests for FactoryGirl support too

--- spec/models/faster_specs_spec.rb
+++ spec/models/faster_specs_spec.rb
@@ -37,29 +37,40 @@ RSpec.describe 'Faster Specs', type: :faster do
     it_behaves_like 'correct behavior'
   end
 
-  context 'meta' do
-    let( :workbench ){ stub_model Workbench }
+  shared_examples_for 'meta' do |org_builder|
     context 'workbench belongs to organisation' do
       it 'workbench has no orgnaisation' do
         expect( workbench.organisation ).to be_nil
       end
       it 'but it can be set' do
-        organisation = stub_model Organisation
+        organisation = stub_model org_builder
         workbench.organisation = organisation
         expect( workbench.organisation ).to eq(organisation)
       end
       it 'and by setting it we get the reverse relation working' do
-        organisation = stub_model Organisation
+        organisation = stub_model org_builder
         workbench.organisation = organisation
         expect( organisation.workbenches ).to be_eql([workbench])
       end
       it 'can also be constructed that way' do
-        organisation = stub_model Organisation
+        organisation = stub_model org_builder
         workbench    = stub_model Workbench, organisation: organisation
         expect( workbench.organisation ).to eq(organisation)
         expect( organisation.workbenches ).to be_eql([workbench])
       end
     end
+  end
+
+  context 'ActiveRecordMeta' do
+    let( :workbench ){ stub_model Workbench }
+
+    it_behaves_like 'meta', Organisation
+  end
+
+  context 'FactoryGirlMeta' do
+    let( :workbench ){ stub_model :workbench }
+
+    it_behaves_like 'meta', :organisation
 
   end
 end
