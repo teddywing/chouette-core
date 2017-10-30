d6213f724633abfa2f899715b6f686d8972fe7e1
Robert <robert.dober@af83.com>
2017-10-30 17:59:26 +0100

    Refs: #4774@2h;
    View correctly implemented, but spex on filters do not work yet.

--- app/helpers/compliance_controls_helper.rb
+++ app/helpers/compliance_controls_helper.rb
@@ -1,2 +1,11 @@
 module ComplianceControlsHelper
+  def subclass_selection_list
+    ComplianceControl.subclass_patterns.map(&method(:make_subclass_selection_item))
+  end
+
+
+  def make_subclass_selection_item(key_pattern)
+    key, pattern = key_pattern
+    [t("compliance_controls.filters.subclasses.#{key}"), "-#{pattern}-"]
+  end
 end 
