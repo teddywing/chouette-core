43033eef0f6582f4bc4e64199310cc624e18f1be
cedricnjanga <ced.njg@gmail.com>
2017-11-06 15:40:33 +0100

    Fix on ImportMessage#criticity enumeration

--- app/models/import_message.rb
+++ app/models/import_message.rb
@@ -2,7 +2,7 @@ class ImportMessage < ActiveRecord::Base
   extend Enumerize
   belongs_to :import
   belongs_to :resource, class_name: ImportResource
-  enumerize :criticity, in: %i(info, warning, error)
+  enumerize :criticity, in: %i(info warning error)
 
   validates :criticity, presence: true
 end
