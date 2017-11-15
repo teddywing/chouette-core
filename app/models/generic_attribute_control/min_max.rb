83cee8ade1c6a9cfd7db724ba60f82f2229e78a2
Robert <robert.dober@af83.com>
2017-11-15 23:55:32 +0100

    Refs: #4800@2h; sketching out an alternative solution

--- app/models/generic_attribute_control/min_max.rb
+++ app/models/generic_attribute_control/min_max.rb
@@ -1,15 +1,38 @@
 module GenericAttributeControl
   class MinMax < ComplianceControl
-    hstore_accessor :control_attributes, minimum: :integer, maximum: :integer, target: :string
+
+    class << self
+      def attribute_type; :integer end
+      def default_code; "3-Generic-2" end
+    end
+
+    hstore_accessor :control_attributes, maximum: :integer, target: :string
 
     validates :minimum, numericality: true, allow_nil: true
     validates :maximum, numericality: true, allow_nil: true
     validates :target, presence: true
     include MinMaxValuesValidation
 
-    class << self
-      def attribute_type; :integer end
-      def default_code; "3-Generic-2" end
+    def minimum
+      control_attributes['_minimum']
     end
+    def minimum= new_value
+      case new_value
+      when String
+        control_attributes['_minimum'] = cast_to_int(:minimum, new_value)
+      else
+        control_attributes['_minimum']= new_value
+      end
+    end
+    
+
+    private
+    
+    def cast_to_int(field, value)
+      return Integer(value)
+    rescue ArgumentError
+      value
+    end
+
   end
 end
--- spec/models/compliance_control_validations/genric_attribute_validation/min_max_validation_spec.rb
+++ spec/models/compliance_control_validations/genric_attribute_validation/min_max_validation_spec.rb
@@ -2,7 +2,15 @@ RSpec.describe GenericAttributeControl::MinMax do
 
   let( :factory ){ :generic_attribute_control_min_max }
   subject{ build factory }
+  context "numerical validation" do
+    it "is valid" do
+      subject.minimum = 42
+      expect_it.to be_valid
+    end
 
-  it_behaves_like 'has min_max_values'
-
+    it "is invalid" do
+      subject.minimum = "42s"
+      expect_it.not_to be_valid
+    end
+  end
 end
