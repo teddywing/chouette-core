30c362f0371c081e0ddb9bc13c5873b1aecda1eb
Robert <robert.dober@af83.com>
2017-11-15 17:15:24 +0100

    Fixes: #4461@2h; Specing and Implementing

--- app/models/netex_import.rb
+++ app/models/netex_import.rb
@@ -1,26 +1,28 @@
 require 'net/http'
 class NetexImport < Import
-  after_commit :launch_java_import, on: :create, unless: :aborted?
+  after_create :launch_java_import
+  before_save def abort_unless_referential
+    self.status = 'aborted' unless referential
+  end
 
   validates_presence_of :parent
 
   def launch_java_import
     return if self.class.finished_statuses.include?(status)
-
-    Thread.new do
-      begin
-        Net::HTTP.get(URI("#{Rails.configuration.iev_url}/boiv_iev/referentials/importer/new?id=#{id}"))
-      rescue Exception => e
-        logger.error "IEV server error : #{e.message}"
-        logger.error e.backtrace.inspect
-      end
-    end
+    threaded_call_boiv_iev
   end
 
 
   private
 
-  def aborted?
-    status == 'aborted'
+  def threaded_call_boiv_iev
+    Thread.new(&method(:call_boiv_iev))
+  end
+
+  def call_boiv_iev
+    Net::HTTP.get(URI("#{Rails.configuration.iev_url}/boiv_iev/referentials/importer/new?id=#{id}"))
+  rescue Exception => e
+    logger.error "IEV server error : #{e.message}"
+    logger.error e.backtrace.inspect
   end
 end
