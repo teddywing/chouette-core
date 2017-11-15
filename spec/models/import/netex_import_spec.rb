2b925fe2bf8f33a0d10dfe1307b086d9e3f0add4
Robert <robert.dober@af83.com>
2017-11-15 17:15:24 +0100

    Fixes: #4461@2h; Specing and Implementing

--- spec/models/import/netex_import_spec.rb
+++ spec/models/import/netex_import_spec.rb
@@ -1,23 +1,28 @@
 RSpec.describe NetexImport, type: :model do
 
+  let( :invoked_calls ){ [] }
+
+  let( :http_service ){ double 'Net::HTTP' }
+  before do
+    stub_const 'Net::HTTP', http_service
+    allow(http_service).to receive( :get ){ invoked_calls << :called }
+  end
 
   context 'with referential' do
+    subject { build :netex_import }
     it 'will trigger the Java API' do
-
+      subject.save
+      expect( invoked_calls ).to eq([:called])
     end
   end
 
   context 'without referential' do
     subject { build :netex_import, referential_id: nil }
 
-    it 'is aborted if it does not have a referential' do
-      require 'pry'; binding.pry
-
-
-    end
-
-    it 'will not trigger the Java API' do
-
+    it 'its status is forced to aborted and the Java API is not callled' do
+      subject.save!
+      expect( subject.reload.status ).to eq('aborted')
+      expect( invoked_calls ).to be_empty
     end
 
   end
