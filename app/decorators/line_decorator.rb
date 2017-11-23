c83002d8f281384244dc9c11432e2eb648b99b3b
Guillaume <guillaume.perot@af83.com>
2017-11-23 17:13:09 +0100

    Refs #4824, #4825 update old form views to new layout with simple_form, add permissions for create an edit in line and network controller

--- app/decorators/line_decorator.rb
+++ app/decorators/line_decorator.rb
@@ -1,45 +1,54 @@
-  class LineDecorator < Draper::Decorator
-    decorates Chouette::Line
+class LineDecorator < Draper::Decorator
+  decorates Chouette::Line
 
-    delegate_all
+  delegate_all
 
-    # Requires:
-    #   context: {
-    #     line_referential: ,
-    #     current_organisation:
-    #   }
-    def action_links
-      links = []
+  # Requires:
+  #   context: {
+  #     line_referential: ,
+  #     current_organisation:
+  #   }
+  def action_links
+    links = []
 
-      links << Link.new(
-        content: h.t('lines.actions.show_network'),
-        href: [context[:line_referential], object.network]
+    links << Link.new(
+      content: h.t('lines.actions.show_network'),
+      href: [context[:line_referential], object.network]
+    )
+
+    links << Link.new(
+      content: h.t('lines.actions.show_company'),
+      href: [context[:line_referential], object.company]
+    )
+    if h.policy(Chouette::Line).create? &&
+      context[:line_referential].organisations.include?(
+        context[:current_organisation]
       )
-
       links << Link.new(
-        content: h.t('lines.actions.show_company'),
-        href: [context[:line_referential], object.company]
+        content: h.t('lines.actions.edit'),
+        href: h.edit_line_referential_line_path(context[:line_referential], object.id)
       )
-
-      if h.policy(Chouette::Line).create? &&
-          context[:line_referential].organisations.include?(
-            context[:current_organisation]
-          )
-        links << Link.new(
-          content: h.t('lines.actions.new'),
-          href: h.new_line_referential_line_path(context[:line_referential])
-        )
-      end
-
-      if h.policy(object).destroy?
-        links << Link.new(
-          content: h.destroy_link_content('lines.actions.destroy_confirm'),
-          href: h.line_referential_line_path(context[:line_referential], object),
-          method: :delete,
-          data: { confirm: h.t('lines.actions.destroy_confirm') }
-        )
-      end
-
-      links
     end
+
+    if h.policy(Chouette::Line).create? &&
+      context[:line_referential].organisations.include?(
+        context[:current_organisation]
+      )
+      links << Link.new(
+        content: h.t('lines.actions.new'),
+        href: h.new_line_referential_line_path(context[:line_referential])
+      )
+    end
+
+    if h.policy(object).destroy?
+      links << Link.new(
+        content: h.destroy_link_content('lines.actions.destroy'),
+        href: h.line_referential_line_path(context[:line_referential], object),
+        method: :delete,
+        data: {confirm: h.t('lines.actions.destroy_confirm')}
+      )
+    end
+
+    links
   end
+end
