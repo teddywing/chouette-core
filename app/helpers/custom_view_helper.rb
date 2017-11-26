bc29c0819764ea64b5f3977458ba4e8e37d59f49
Alban Peignier <alban.peignier@af83.com>
2017-11-26 17:16:35 +0100

    Use debug message for render_custom_view. Refs #4735

--- app/helpers/custom_view_helper.rb
+++ app/helpers/custom_view_helper.rb
@@ -2,7 +2,7 @@ module CustomViewHelper
 
   def render_custom_view(view)
     view_name = [view, current_organisation.custom_view].compact.join('_')
-    Rails.logger.info "Render custom view #{view_name}"
+    Rails.logger.debug "Render custom view #{view_name}"
     render partial: view_name
   end
 
