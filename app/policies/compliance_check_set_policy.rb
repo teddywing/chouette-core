17e946771afcff10b6165dd3a97b38aa9c06b1f2
Luc Donnet <luc.donnet@af83.com>
2017-11-28 12:07:16 +0100

    Fix import policy to not authorize destroy and not be linked with referential state (archived) Refs #5093

--- app/policies/compliance_check_set_policy.rb
+++ app/policies/compliance_check_set_policy.rb
@@ -3,5 +3,17 @@ class ComplianceCheckSetPolicy < ApplicationPolicy
     def resolve
       scope
     end
+
+      def create?
+        false # ComplianceCheckSet can not be created from controller
+      end
+
+      def destroy?
+        false # Asynchronous operations must not be deleted
+      end
+
+      def update?
+        false # ComplianceCheckSet can not be updated from controller
+      end
   end
 end
