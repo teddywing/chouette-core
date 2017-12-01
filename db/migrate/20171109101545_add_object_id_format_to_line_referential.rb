1bdb0765df3a172daedaa0e86dd1cdce328489fb
Luc Donnet <luc.donnet@af83.com>
2017-12-01 12:35:00 +0100

    Fix objectid_format creation in migration with existence test.

--- db/migrate/20171109101545_add_object_id_format_to_line_referential.rb
+++ db/migrate/20171109101545_add_object_id_format_to_line_referential.rb
@@ -1,5 +1,5 @@
 class AddObjectIdFormatToLineReferential < ActiveRecord::Migration
   def change
-    add_column :line_referentials, :objectid_format, :string
+    add_column :line_referentials, :objectid_format, :string unless column_exists? :line_referentials, :objectid_format
   end
 end
