1d5a19ce9f4f63fdb82cc1c985c9433495fd4920
cedricnjanga <ced.njg@gmail.com>
2017-12-04 17:06:18 +0100

    Small refacto to handle no_parent error for ComplianceCheckSet

--- app/controllers/api/v1/internals/application_controller.rb
+++ app/controllers/api/v1/internals/application_controller.rb
@@ -2,16 +2,13 @@ module Api
   module V1
     module Internals
       class ApplicationController < ActionController::Base
-        inherit_resources
         respond_to :json
         layout false
         before_action :authenticate
 
         private
         def authenticate
-          authenticate_with_http_token do |token, options|
-            @current_organisation = Api::V1::ApiKey.find_by_token(token).try(:organisation)
-          end
+          authenticate_with_http_token { |token| Rails.application.secrets.api_token == token }
         end
       end
     end
