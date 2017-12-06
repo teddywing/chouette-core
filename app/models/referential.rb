af709926b6fac4fce22345a34fdf57de5b4d6cbf
Luc Donnet <luc.donnet@af83.com>
2017-12-06 14:12:14 +0100

    Fix log to detect schema migrations overwrite

--- app/models/referential.rb
+++ app/models/referential.rb
@@ -288,8 +288,8 @@ class Referential < ActiveRecord::Base
   def create_schema
     unless created_from
       Apartment::Tenant.create slug
+      Rails.logger.error( "Schema migrations count for Referential #{slug} " + Referential.connection.select_value("select count(*) from #{slug}.schema_migrations;").to_s )
     end
-    Rails.logger.error( "Schema migrations count for Referential #{slug} " + Referential.connection.select_value("select count(*) from #{slug}.schema_migrations;").to_s )
   end
 
   def assign_slug
