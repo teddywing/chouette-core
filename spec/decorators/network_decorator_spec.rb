5e13c90c154b71a5ae385f3e6cf262bb8112b08e
Xinhui <xinhui.xu@af83.com>
2017-12-07 14:33:45 +0100

    Network decorator policy spec
    Refs #5150

---
+++ spec/decorators/network_decorator_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe NetworkDecorator do
+  let(:object) { build_stubbed :network }
+  let(:user) { build_stubbed :user }
+
+  describe 'action links' do
+    before do
+      allow(helpers).to receive(:edit_line_referential_network_path).and_return('/edit_path')
+      allow(helpers).to receive(:new_line_referential_network_path).and_return('/new_path')
+      allow(helpers).to receive(:line_referential_network_path).and_return('/show_path')
+      allow(helpers).to receive(:destroy_link_content).and_return('destroy_link_content')
+    end
+
+    context 'for authorized user' do
+      before do
+        stub_policy([create?: true, update?: true, destroy?: true])
+      end
+
+      it 'should render create link' do
+        expect(subject.action_links.map(&:content)).to include(I18n.t('networks.actions.new'))
+        expect_action_link_hrefs.to include('/new_path')
+      end
+
+      it 'should render edit link' do
+        expect(subject.action_links.map(&:content)).to include(I18n.t('networks.actions.edit'))
+        expect_action_link_hrefs.to include('/edit_path')
+      end
+
+      it 'should render destroy link' do
+        expect(subject.action_links.map(&:content)).to include('destroy_link_content')
+      end
+    end
+
+    context 'for unauthorized user' do
+      before do
+        stub_policy([create?: false, update?: false, destroy?: false])
+      end
+
+      it 'should not render create link' do
+        expect(subject.action_links.map(&:content)).to_not include(I18n.t('networks.actions.new'))
+        expect_action_link_hrefs.to_not include('/new_path')
+      end
+
+      it 'should not render edit link' do
+        expect(subject.action_links.map(&:content)).to_not include(I18n.t('networks.actions.edit'))
+        expect_action_link_hrefs.to_not include('/edit_path')
+      end
+
+      it 'should not render destroy link' do
+        expect(subject.action_links.map(&:content)).to_not include('destroy_link_content')
+      end
+    end
+  end
+end
