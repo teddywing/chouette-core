9bde9a3d24e3893a7a4ee89c95044899e59c89f4
Robert <robert.dober@af83.com>
2017-12-07 11:15:01 +0100

    Refs #5006@0.5h; CR pass II

--- lib/stif/codifligne_line_id.rb
+++ lib/stif/codifligne_line_id.rb
@@ -3,14 +3,17 @@ module STIF
 
     LINE_OBJECT_ID_SEPERATOR = ':'
 
-    def extract_codif_line_id line_name
-      line_name.split(LINE_OBJECT_ID_SEPERATOR).last
-    end
-
     def lines_set_from_functional_scope(functional_scope)
       Set.new(
         functional_scope
           .map{ |line| extract_codif_line_id line })
+    end
+
+
+    private
+
+    def extract_codif_line_id line_name
+      line_name.split(LINE_OBJECT_ID_SEPERATOR).last
     end
   end
 end
