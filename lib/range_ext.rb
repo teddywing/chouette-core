f90488de1f657abf24026231485c87d3e42ee11d
Alban Peignier <alban.peignier@af83.com>
2017-12-17 15:31:47 +0100

    Move (clean) period split logic into Range#remove. Refs #5299

--- lib/range_ext.rb
+++ lib/range_ext.rb
@@ -5,4 +5,16 @@ class Range
     [self.min, other.min].max..[self.max, other.max].min
   end
   alias_method :&, :intersection
+
+  def remove(other)
+    return self if (self.max < other.min or other.max < self.min)
+
+    [].tap do |remaining|
+      remaining << (self.min..other.min-1) if self.min < other.min
+      remaining << (other.max+1..self.max) if other.max < self.max
+      remaining.compact!
+    end
+  end
+  alias_method :-, :remove
+
 end
--- spec/lib/range_ext_spec.rb
+++ spec/lib/range_ext_spec.rb
@@ -15,4 +15,27 @@ RSpec.describe Range do
       expect( (2..4) & (1..3) ).to eq 2..3
     end
   end
+
+  context "remove" do
+    it "is unchanged when the given range has no intersection" do
+      expect( (1..2).remove(3..4) ).to eq 1..2
+      expect( (3..4).remove(1..2) ).to eq 3..4
+    end
+
+    it "is nil for two equal ranges" do
+      expect( (1..2).remove(1..2) ).to be_empty
+    end
+
+    it "is the begin of the range when given range intersect the end" do
+      expect( (5..10).remove(8..15) ).to eq [5..7]
+    end
+
+    it "is the end of the range when given range intersect the begin" do
+      expect( (5..10).remove(1..6) ).to eq [7..10]
+    end
+
+    it "is the two remaing ranges when given range is the middle" do
+      expect( (1..10).remove(4..6) ).to eq [1..3, 7..10]
+    end
+  end
 end
