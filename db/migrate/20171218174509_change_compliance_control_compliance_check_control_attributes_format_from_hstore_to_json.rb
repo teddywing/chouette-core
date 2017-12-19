908058cca1cf7340e2b6b201dd03e8896a277e28
Teddy Wing <teddy.wing@af83.com>
2017-12-19 17:32:26 +0100

    Add `down` to 20171218174509 Hstore to JSON migration
    Copy Cédric's JSON to Hstore migration for the `down` here to give us
    the ability to roll back.
    
    I'm guessing the solution probably comes from here:
    https://stackoverflow.com/questions/28315948/casting-json-to-hstore-in-postgres-9-3/28319295#28319295
    
    Refs #5316

--- db/migrate/20171218174509_change_compliance_control_compliance_check_control_attributes_format_from_hstore_to_json.rb
+++ db/migrate/20171218174509_change_compliance_control_compliance_check_control_attributes_format_from_hstore_to_json.rb
@@ -1,10 +1,33 @@
 class ChangeComplianceControlComplianceCheckControlAttributesFormatFromHstoreToJson < ActiveRecord::Migration
-  def change
+  def up
     change_column :compliance_controls,
       :control_attributes,
       'json USING hstore_to_json(control_attributes)'
     change_column :compliance_checks,
       :control_attributes,
       'json USING hstore_to_json(control_attributes)'
+  end
+
+  def down
+    execute <<-SQL
+      CREATE OR REPLACE FUNCTION my_json_to_hstore(json)
+            RETURNS hstore
+            IMMUTABLE
+            STRICT
+            LANGUAGE sql
+          AS $func$
+            SELECT hstore(array_agg(key), array_agg(value))
+            FROM   json_each_text($1)
+          $func$;
+    SQL
+
+    change_column :compliance_controls,
+      :control_attributes,
+      'hstore USING my_json_to_hstore(control_attributes)'
+    change_column :compliance_checks,
+      :control_attributes,
+      'hstore USING my_json_to_hstore(control_attributes)'
+
+    execute "DROP FUNCTION my_json_to_hstore(json)"
   end
 end
