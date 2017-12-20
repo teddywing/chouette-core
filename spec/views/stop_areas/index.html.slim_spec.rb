49a515b474cf64b02f6dfc7a449f3a45aa21e7ca
Zog <johan@about-blank.fr>
2017-12-20 08:12:20 +0100

    Fix spec after rebase

--- spec/views/stop_areas/index.html.slim_spec.rb
+++ spec/views/stop_areas/index.html.slim_spec.rb
@@ -20,8 +20,8 @@ describe "/stop_areas/index", :type => :view do
   it { should have_the_right_number_of_links(stop_areas, 1) }
 
   with_permission "stop_areas.create" do
-    it { should have_link_for_each_stop_area(stop_areas, "show", -> (stop_area){ view.stop_area_referential_stop_area_path(stop_area_referential, stop_area) }) }
-    it { should_not have_link_for_each_stop_area(stop_areas, "create", -> (stop_area){ view.new_stop_area_referential_stop_area_path(stop_area_referential) }) }
+    it { should have_link_for_each_item(stop_areas, "show", -> (stop_area){ view.stop_area_referential_stop_area_path(stop_area_referential, stop_area) }) }
+    it { should_not have_link_for_each_item(stop_areas, "create", -> (stop_area){ view.new_stop_area_referential_stop_area_path(stop_area_referential) }) }
     it { should have_the_right_number_of_links(stop_areas, 1) }
   end
 
