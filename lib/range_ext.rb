7a65a9fbe0cefdf504e7d735003065d8bb874f1e
Alban Peignier <alban.peignier@af83.com>
2017-12-27 21:23:07 +0100

    Add Range#intersect?. Refs #5299

--- lib/range_ext.rb
+++ lib/range_ext.rb
@@ -1,10 +1,14 @@
 class Range
 
   def intersection(other)
-    return nil if (self.max < other.min or other.max < self.min)
+    return nil unless intersect?(other)
     [self.min, other.min].max..[self.max, other.max].min
   end
   alias_method :&, :intersection
+
+  def intersect?(other)
+    self.max > other.min and other.max > self.min
+  end
 
   def remove(other)
     return self if (self.max < other.min or other.max < self.min)
