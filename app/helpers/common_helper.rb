3cd00c8b27e35028593479b717bfa01efe937b0c
Alban Peignier <alban.peignier@af83.com>
2017-12-29 11:16:52 +0100

    Replace CommonHelper.string_keys_to_symbols by Hash#symbolize_keys

--- app/helpers/common_helper.rb
+++
@@ -1,8 +0,0 @@
-
-module CommonHelper
-  def string_keys_to_symbols enumerable_pairs
-    enumerable_pairs.inject({}){ | hashy, (k, v) |
-      hashy.merge k.to_sym => v
-    }
-  end
-end
--- spec/views/imports/show.html.slim_spec.rb
+++ spec/views/imports/show.html.slim_spec.rb
@@ -31,11 +31,8 @@ RSpec.describe '/imports/show', type: :view do
     end
   end
 
-
   def rendered_message message
-    Object.new.extend(CommonHelper).tap do |helper|
-      return I18n.t(message.message_key, helper.string_keys_to_symbols( message.message_attributes ))
-    end
+    return I18n.t message.message_key, message.message_attributes.symbolize_keys
   end
 
 end
