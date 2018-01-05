31b7b91b46d2e8f740d2cfd493836219f5a4f42d
Xinhui <xinhui.xu@af83.com>
2018-01-05 16:43:37 +0100

    Refactoring use subject as factory
    Refs #5416

--- spec/support/checksum_support.rb
+++ spec/support/checksum_support.rb
@@ -1,17 +1,15 @@
-shared_examples 'checksum support' do |factory_name|
-  let(:instance) { create(factory_name) }
-
+shared_examples 'checksum support' do
   describe '#current_checksum_source' do
     let(:attributes) { ['code_value', 'label_value'] }
     let(:seperator)  { ChecksumSupport::SEPARATOR }
     let(:nil_value)  { ChecksumSupport::VALUE_FOR_NIL_ATTRIBUTE }
 
     before do
-      allow_any_instance_of(instance.class).to receive(:checksum_attributes).and_return(attributes)
+      allow_any_instance_of(subject.class).to receive(:checksum_attributes).and_return(attributes)
     end
 
     it 'should separate attribute by seperator' do
-      expect(instance.current_checksum_source).to eq("code_value#{seperator}label_value")
+      expect(subject.current_checksum_source).to eq("code_value#{seperator}label_value")
     end
 
     context 'nil value' do
@@ -19,7 +17,7 @@ shared_examples 'checksum support' do |factory_name|
 
       it 'should replace nil attributes by default value' do
         source = "code_value#{seperator}#{nil_value}"
-        expect(instance.current_checksum_source).to eq(source)
+        expect(subject.current_checksum_source).to eq(source)
       end
     end
 
@@ -28,26 +26,26 @@ shared_examples 'checksum support' do |factory_name|
 
       it 'should convert to nil' do
         source = "code_value#{seperator}#{nil_value}"
-        expect(instance.current_checksum_source).to eq(source)
+        expect(subject.current_checksum_source).to eq(source)
       end
     end
   end
 
   it 'should save checksum on create' do
-    expect(instance.checksum).to_not be_nil
+    expect(subject.checksum).to_not be_nil
   end
 
   it 'should save checksum_source' do
-    expect(instance.checksum_source).to_not be_nil
+    expect(subject.checksum_source).to_not be_nil
   end
 
   it 'should trigger set_current_checksum_source on save' do
-    expect(instance).to receive(:set_current_checksum_source).at_least(:once)
-    instance.save
+    expect(subject).to receive(:set_current_checksum_source).at_least(:once)
+    subject.save
   end
 
   it 'should trigger update_checksum on save' do
-    expect(instance).to receive(:update_checksum).at_least(:once)
-    instance.save
+    expect(subject).to receive(:update_checksum).at_least(:once)
+    subject.save
   end
 end
