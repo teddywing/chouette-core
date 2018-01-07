0e41c4b6d234ac0be28afbfd90955648816e6275
Alban Peignier <alban.peignier@af83.com>
2018-01-07 22:37:39 +0100

    Add created organisation in Line/StopAreaReferential. Refs #5487

--- config/initializers/stif.rb
+++ config/initializers/stif.rb
@@ -3,6 +3,9 @@ Rails.application.config.to_prepare do
     line_referential      = LineReferential.find_by(name: "CodifLigne")
     stop_area_referential = StopAreaReferential.find_by(name: "Reflex")
 
+    line_referential.organisations << organisation
+    stop_area_referential.organisations << organisation
+
     organisation.workbenches.find_or_create_by(name: "Gestion de l'offre") do |workbench|
       workbench.line_referential      = line_referential
       workbench.stop_area_referential = stop_area_referential
