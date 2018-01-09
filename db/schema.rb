e38c60d149bc561e35bde60c532782fd41adc22b
cedricnjanga <ced.njg@gmail.com>
2018-01-09 15:22:26 +0100

    Commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105102012) do
+ActiveRecord::Schema.define(version: 20180103084612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
   enable_extension "postgis"
+  enable_extension "hstore"
   enable_extension "unaccent"
 
   create_table "access_links", id: :bigserial, force: :cascade do |t|
