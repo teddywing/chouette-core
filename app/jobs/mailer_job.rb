e486aa5f3375a1e41ab7db9abda778f89e457e21
Teddy Wing <teddy.wing@af83.com>
2018-01-09 15:06:47 +0100

    MailerJob: Add comment describing retry behaviour
    Now that all other workers don't retry automatically (see
    3487ce6f34ef187a2a8daf170440f51f659c46b7), add a comment here to clarify
    that this worker is different, and we explicitly want it to retry.
    
    The disabling of retries only affects Sidekiq jobs.
    
    Refs #5495

--- app/jobs/mailer_job.rb
+++ app/jobs/mailer_job.rb
@@ -1,6 +1,9 @@
 class MailerJob < ActiveJob::Base
   # No need to specify queue it's already used mailers queue
 
+  # This job will be retried, unlike Sidekiq jobs which are configured
+  # to not retry
+
   def perform klass, action, params
     klass.constantize.public_send(action, *params).deliver_later
   end
