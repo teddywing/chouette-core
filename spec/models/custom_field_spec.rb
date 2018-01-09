fd553851e9e6c59b998eef8577836a954c4d43f3
Robert <robert.dober@af83.com>
2018-01-09 17:46:20 +0100

    Refs: #5505@0.5h, added tests and validations

--- spec/models/custom_field_spec.rb
+++ spec/models/custom_field_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CustomField, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "validates" do
+    it { should validate_uniqueness_of(:name).scoped_to(:resource_type) }
+  end
+
+  context "field access" do
+    let( :custom_field ){ build_stubbed :custom_field }
+
+    it "option's values can be accessed by a key" do
+      expect( custom_field.options['capacity'] ).to eq("0")
+    end
+
+  end
 end
