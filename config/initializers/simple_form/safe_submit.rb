db40a5f1eada6aa4f000c0ffbe6c93e3aa18fe1d
Zog <johan.van-ryseghem@af83.com>
2018-01-10 12:37:19 +0100

    Refs #5525; Refactor

--- config/initializers/simple_form/safe_submit.rb
+++ config/initializers/simple_form/safe_submit.rb
@@ -1,7 +1,12 @@
-AF83::SimpleForm::SafeSubmit.decorate_simple_form
-
-if Rails.env.development?
-  ActionDispatch::Reloader.to_prepare do
-    AF83::SimpleForm::SafeSubmit.decorate_simple_form
+::SimpleForm::FormBuilder.class_eval do
+  def button_with_safe_submit(type, *args, &block)
+    options = args.extract_options!.dup
+    if type == :submit
+      options[:data] ||= {}
+      options[:data][:disable_with] ||= I18n.t('actions.wait_for_submission')
+    end
+    args << options
+    button_without_safe_submit type, *args, &block
   end
+  alias_method_chain :button, :safe_submit
 end
