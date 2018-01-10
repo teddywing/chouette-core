c75d10e23b23b3ef3fa4bcbd380c05c59254a393
Alban Peignier <alban.peignier@af83.com>
2018-01-10 15:45:03 +0100

    Implement StopArea#country_name. Refs #5156

--- app/models/chouette/stop_area.rb
+++ app/models/chouette/stop_area.rb
@@ -357,8 +357,10 @@ module Chouette
     end
 
     def country_name
-      # XXX
-      country_code
+      return unless country_code
+
+      country = ISO3166::Country[country_code]
+      country.translations[I18n.locale.to_s] || country.name
     end
   end
 end
