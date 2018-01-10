6ea5fe58761b2549def5249b769dfea9df3b4fde
Alban Peignier <alban.peignier@af83.com>
2018-01-10 17:32:51 +0100

    Use a has_many through for Workgroup#organisations. Refs #5499

--- app/models/workgroup.rb
+++ app/models/workgroup.rb
@@ -3,13 +3,10 @@ class Workgroup < ActiveRecord::Base
   belongs_to :stop_area_referential
 
   has_many :workbenches
+  has_many :organisations, through: :workbenches
 
   validates_uniqueness_of :name
 
   validates_presence_of :line_referential_id
   validates_presence_of :stop_area_referential_id
-
-  def organisations
-    workbenches.map(&:organisation)
-  end
 end
