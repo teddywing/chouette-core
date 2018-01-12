41d2f53af34457c66cf3d504ac1bba610d11d596
Zog <johan.van-ryseghem@af83.com>
2018-01-12 09:23:06 +0100

    Refs #5560; Add validation on CustomField

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -1,7 +1,8 @@
 class CustomField < ActiveRecord::Base
 
   extend Enumerize
-  enumerize :field_type, in: %i{list} 
+  enumerize :field_type, in: %i{list}
 
   validates :name, uniqueness: {scope: :resource_type}
+  validates :code, uniqueness: {scope: :resource_type, case_sensitive: false}
 end
