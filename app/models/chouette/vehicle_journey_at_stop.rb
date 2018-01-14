4336567b63bba12b12d3fdf32595d166a8c0e5fb
Alban Peignier <alban.peignier@af83.com>
2018-01-14 23:13:56 +0100

    Fixes VehicleJourneyAtStop#local_time to prevent error

--- app/models/chouette/vehicle_journey_at_stop.rb
+++ app/models/chouette/vehicle_journey_at_stop.rb
@@ -102,6 +102,7 @@ module Chouette
 
     private
     def local_time time
+      return unless time
       return time unless stop_point&.stop_area&.time_zone.present?
       time + ActiveSupport::TimeZone[stop_point.stop_area.time_zone].utc_offset
     end
