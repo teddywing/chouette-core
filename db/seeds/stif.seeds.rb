02ac58587782729ab68da0d7e3f59d3025243175
Luc Donnet <luc.donnet@af83.com>
2018-01-22 14:32:50 +0100

    Comment user and operator A creation in stif seed

--- db/seeds/stif.seeds.rb
+++ db/seeds/stif.seeds.rb
@@ -9,30 +9,30 @@ line_referential = LineReferential.find_or_create_by!(name: "CodifLigne", object
 stif = Organisation.find_or_create_by!(code: "STIF") do |org|
   org.name = 'STIF'
 end
-operator = Organisation.find_or_create_by!(code: 'transporteur-a') do |organisation|
-  organisation.name = "Transporteur A"
-end
+# operator = Organisation.find_or_create_by!(code: 'transporteur-a') do |organisation|
+#   organisation.name = "Transporteur A"
+# end
 
 # Member
 line_referential.add_member stif, owner: true
-line_referential.add_member operator
+# line_referential.add_member operator
 
 stop_area_referential.add_member stif, owner: true
-stop_area_referential.add_member operator
+# stop_area_referential.add_member operator
 
 # Users
-stif.users.find_or_create_by!(username: "admin") do |user|
-  user.email = 'stif-boiv@af83.com'
-  user.password = "secret"
-  user.name = "STIF Administrateur"
-end
-
-operator.users.find_or_create_by!(username: "transporteur") do |user|
-  user.email = 'stif-boiv+transporteur@af83.com'
-  user.password = "secret"
-  user.name = "Martin Lejeune"
-end
+# stif.users.find_or_create_by!(username: "admin") do |user|
+#   user.email = 'stif-boiv@af83.com'
+#   user.password = "secret"
+#   user.name = "STIF Administrateur"
+# end
+#
+# operator.users.find_or_create_by!(username: "transporteur") do |user|
+#   user.email = 'stif-boiv+transporteur@af83.com'
+#   user.password = "secret"
+#   user.name = "Martin Lejeune"
+# end
 
 # Include all Lines in organisation functional_scope
 stif.update sso_attributes: { functional_scope: line_referential.lines.pluck(:objectid) }
-operator.update sso_attributes: { functional_scope: line_referential.lines.limit(3).pluck(:objectid) }
+#operator.update sso_attributes: { functional_scope: line_referential.lines.limit(3).pluck(:objectid) }
