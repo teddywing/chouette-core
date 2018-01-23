fabfdd6c2188c8415b18760324df3a099e560df5
Zog <johan.van-ryseghem@af83.com>
2018-01-23 14:31:26 +0100

    Refs #5669; And more snapshots

--- spec/views/companies/show.html.erb_spec.rb
+++ spec/views/companies/show.html.erb_spec.rb
@@ -9,4 +9,28 @@ describe "/companies/show", :type => :view do
   #   render
   #  expect(rendered).to have_selector("#map", :class => 'company')
   # end
+
+  before(:each) do
+    allow(view).to receive(:current_referential).and_return(line_referential)
+    allow(view).to receive(:resource).and_return(company)
+    controller.request.path_parameters[:line_referential_id] = line_referential.id
+    controller.request.path_parameters[:id] = company.id
+  end
+
+  describe "action links" do
+    set_invariant "line_referential.id", "99"
+    set_invariant "company.id", "909"
+
+    before(:each){
+      render template: "companies/show", layout: "layouts/application"
+    }
+
+    it { should match_actions_links_snapshot "companies/show" }
+
+    %w(create update destroy).each do |p|
+      with_permission "companies.#{p}" do
+        it { should match_actions_links_snapshot "companies/show_#{p}" }
+      end
+    end
+  end
 end
