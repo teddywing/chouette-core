3e6f023f87984894c3d0c7e4d70ea65c8966f063
Zog <johan.van-ryseghem@af83.com>
2018-01-23 14:31:26 +0100

    Refs #5669; And more snapshots

--- spec/views/networks/show.html.erb_spec.rb
+++ spec/views/networks/show.html.erb_spec.rb
@@ -11,8 +11,27 @@ describe "/networks/show", :type => :view do
   let!(:map) { assign(:map, double(:to_html => '<div id="map"/>'.html_safe)) }
   let!(:line_referential) { assign :line_referential, network.line_referential }
 
-  # it "should display a map with class 'network'" do
-  #   render
-  #   expect(rendered).to have_selector("#map")
-  # end
+  before(:each) do
+    allow(view).to receive(:current_referential).and_return(line_referential)
+    allow(view).to receive(:resource).and_return(network)
+    controller.request.path_parameters[:line_referential_id] = line_referential.id
+    controller.request.path_parameters[:id] = network.id
+  end
+
+  describe "action links" do
+    set_invariant "line_referential.id", "99"
+    set_invariant "network.id", "909"
+
+    before(:each){
+      render template: "networks/show", layout: "layouts/application"
+    }
+
+    it { should match_actions_links_snapshot "networks/show" }
+
+    %w(create update destroy).each do |p|
+      with_permission "networks.#{p}" do
+        it { should match_actions_links_snapshot "networks/show_#{p}" }
+      end
+    end
+  end
 end
