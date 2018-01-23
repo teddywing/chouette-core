c39a3745507fa510eea14b0884a0929bd4729a97
Teddy Wing <teddy.wing@af83.com>
2018-01-23 14:57:07 +0100

    NetworkDecorator: Convert to new action links interface
    And remove the "create" link from the gear menu and header links of the
    :show page because it didn't make sense there. Leave a "create" link on
    the index page though.
    
    Refs #5586

--- app/decorators/network_decorator.rb
+++ app/decorators/network_decorator.rb
@@ -1,44 +1,43 @@
-class NetworkDecorator < Draper::Decorator
+class NetworkDecorator < AF83::Decorator
   decorates Chouette::Network
 
-  delegate_all
-
-  # Requires:
+  # Action links require:
   #   context: {
   #     line_referential: ,
   #   }
-  def action_links
-    links = []
 
-    if h.policy(Chouette::Network).create?
-      links << Link.new(
-        content: h.t('networks.actions.new'),
-        href: h.new_line_referential_network_path(context[:line_referential])
-      )
+  create_action_link do |l|
+    l.content t('networks.actions.new')
+    l.href { h.new_line_referential_network_path(context[:line_referential]) }
+    l.class 'btn btn-primary'
+  end
+
+  with_instance_decorator do |instance_decorator|
+    instance_decorator.show_action_link do |l|
+      l.href do
+        h.line_referential_network_path(context[:line_referential], object)
+      end
     end
 
-    if h.policy(object).update?
-      links << Link.new(
-        content: h.t('networks.actions.edit'),
-        href: h.edit_line_referential_network_path(
+    instance_decorator.action_link secondary: true, policy: :edit do |l|
+      l.content t('networks.actions.edit')
+      l.href do
+        h.edit_line_referential_network_path(
           context[:line_referential],
           object
         )
-      )
+      end
     end
 
-    if h.policy(object).destroy?
-      links << Link.new(
-        content: h.destroy_link_content('networks.actions.destroy'),
-        href: h.line_referential_network_path(
+    instance_decorator.destroy_action_link do |l|
+      l.content h.destroy_link_content('networks.actions.destroy')
+      l.href do
+        h.line_referential_network_path(
           context[:line_referential],
           object
-        ),
-        method: :delete,
-        data: { confirm: h.t('networks.actions.destroy_confirm') }
-      )
+        )
+      end
+      l.data confirm: h.t('networks.actions.destroy_confirm')
     end
-
-    links
   end
 end
