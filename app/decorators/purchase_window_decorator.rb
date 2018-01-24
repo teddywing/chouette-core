12c8ee67fbed944d643b86b8cdf3f7e9fcc4d9cc
Teddy Wing <teddy.wing@af83.com>
2018-01-24 17:01:42 +0100

    PurchaseWindowDecorator: Convert to new action links interface
    Also add a new method `AF83::Decorator.define_instance_method`. Thanks
    to Johan for suggesting that and walking me through writing it. This new
    method allows us to define methods on the instance decorator. We need
    this to preserve the `#bounding_dates` method that was defined in
    `PurchaseWindowDecorator`.
    
    Refs #5586

--- app/decorators/purchase_window_decorator.rb
+++ app/decorators/purchase_window_decorator.rb
@@ -1,31 +1,37 @@
-class PurchaseWindowDecorator < Draper::Decorator
+class PurchaseWindowDecorator < AF83::Decorator
   decorates Chouette::PurchaseWindow
-  delegate_all
 
-  def action_links
-    policy = h.policy(object)
-    links = []
-
-    if policy.update?
-      links << Link.new(
-        content: I18n.t('actions.edit'),
-        href: h.edit_referential_purchase_window_path(context[:referential].id, object)
-      )
-    end
-
-    if policy.destroy?
-      links << Link.new(
-        content: I18n.t('actions.destroy'),
-        href: h.referential_purchase_window_path(context[:referential].id, object),
-        method: :delete,
-        data: { confirm: h.t('purchase_windows.actions.destroy_confirm') }
-      )
-    end
-
-    links
+  create_action_link do |l|
+    l.content t('purchase_windows.actions.new')
+    l.href { h.new_referential_purchase_window_path(context[:referential]) }
   end
 
-  def bounding_dates
+  with_instance_decorator do |instance_decorator|
+    instance_decorator.show_action_link do |l|
+      l.content t('purchase_windows.actions.show')
+      l.href do
+        h.referential_purchase_window_path(
+          context[:referential],
+          object
+        )
+      end
+    end
+
+    instance_decorator.edit_action_link do |l|
+      l.href do
+        h.edit_referential_purchase_window_path(context[:referential].id, object)
+      end
+    end
+
+    instance_decorator.destroy_action_link do |l|
+      l.href do
+        h.referential_purchase_window_path(context[:referential].id, object)
+      end
+      l.data confirm: h.t('purchase_windows.actions.destroy_confirm')
+    end
+  end
+
+  define_instance_method :bounding_dates do
     unless object.date_ranges.empty?
       object.date_ranges.map(&:min).min..object.date_ranges.map(&:max).max
     end
