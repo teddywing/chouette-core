37f77f03c15269a82bdd487fd36458f341536baa
Zog <johan.van-ryseghem@af83.com>
2018-01-24 16:56:16 +0100

    Refs #5682 @3h; Use same UI as for timetables

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -50,7 +50,6 @@ class Calendar < ActiveRecord::Base
         tt.periods << Chouette::TimeTablePeriod.new(period_start: p.begin, period_end: p.end)
       end
       tt.int_day_types = self.int_day_types
-<<<<<<< HEAD
     end
   end
 
@@ -88,8 +87,6 @@ class Calendar < ActiveRecord::Base
   def all_dates
     (dates + excluded_dates).sort.each_with_index.map do |d, i|
       OpenStruct.new(id: i, date: d, in_out: include_in_dates?(d))
-=======
->>>>>>> Refs #5682; Add application_days field to calendars
     end
   end
 
