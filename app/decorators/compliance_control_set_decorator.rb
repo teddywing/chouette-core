d1951f2c2a30baf47e4e84325b9e39437d983e33
Teddy Wing <teddy.wing@af83.com>
2018-01-25 16:30:48 +0100

    ComplianceControlSetDecorator: Remove unnecessary `href`s
    Johan pointed out
    (https://github.com/af83/stif-boiv/commit/cd2892277df0af1e1eaf2373adf0068a77f07a6e)
    that these `href`s aren't needed as the action links decorator will
    infer them automatically.
    
    Refs #5586

--- app/decorators/compliance_control_set_decorator.rb
+++ app/decorators/compliance_control_set_decorator.rb
@@ -3,13 +3,11 @@ class ComplianceControlSetDecorator < AF83::Decorator
 
   create_action_link do |l|
     l.content t('compliance_control_sets.actions.new')
-    l.href h.new_compliance_control_set_path
   end
 
   with_instance_decorator do |instance_decorator|
     instance_decorator.edit_action_link do |l|
       l.content t('compliance_control_sets.actions.edit')
-      l.href { h.edit_compliance_control_set_path(object.id) }
     end
 
     instance_decorator.action_link policy: :clone, secondary: :show do |l|
