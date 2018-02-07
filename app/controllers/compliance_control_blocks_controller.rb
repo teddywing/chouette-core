5cabaff1074373a2387760634a49e2d2b56faee9
cedricnjanga <ced.njg@gmail.com>
2018-02-06 21:03:35 -0800

    Refs #5855 Avoid creation of CCBlocks duplicates in the same CCSet

--- app/controllers/compliance_control_blocks_controller.rb
+++ app/controllers/compliance_control_blocks_controller.rb
@@ -4,6 +4,14 @@ class ComplianceControlBlocksController < ChouetteController
   belongs_to :compliance_control_set
   actions :all, :except => [:show, :index]
 
+  after_action :check_duplicate, only: :create
+
+  def check_duplicate
+    unless @compliance_control_block.errors[:duplicate].empty?
+      flash[:error] = @compliance_control_block.errors[:duplicate].first
+    end
+  end
+
   private
 
   def compliance_control_block_params
