d9c5cdeb76c2e6df0711d6c1b538a583d42bff45
cedricnjanga <ced.njg@gmail.com>
2018-02-07 12:58:44 -0800

    Refs #5815 Add required fields to some compliance controls

--- app/models/vehicle_journey_control/waiting_time.rb
+++ app/models/vehicle_journey_control/waiting_time.rb
@@ -3,6 +3,7 @@ module VehicleJourneyControl
     store_accessor :control_attributes, :maximum
 
     validates_numericality_of :maximum, allow_nil: true, greater_than_or_equal_to: 0
+    validates_presence_of :maximum
 
     def self.default_code; "3-VehicleJourney-1" end
   end
