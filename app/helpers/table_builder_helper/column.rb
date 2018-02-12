b090709fa414939f8c561e9d14bcbb378015c67e
Zog <johan.van-ryseghem@af83.com>
2018-02-12 11:18:10 +0100

    Refs #5877; Add a condition on TableBuilderHelper::Column

--- app/helpers/table_builder_helper/column.rb
+++ app/helpers/table_builder_helper/column.rb
@@ -2,19 +2,21 @@ module TableBuilderHelper
   class Column
     attr_reader :key, :name, :attribute, :sortable
 
-    def initialize(key: nil, name: '', attribute:, sortable: true, link_to: nil)
+    def initialize(key: nil, name: '', attribute:, sortable: true, link_to: nil, **opts)
       if key.nil? && name.empty?
         raise ColumnMustHaveKeyOrNameError
       end
-
+      opts ||= {}
       @key = key
       @name = name
       @attribute = attribute
       @sortable = sortable
       @link_to = link_to
+      @condition = opts[:if]
     end
 
     def value(obj)
+      return unless check_condition(obj)
       if @attribute.is_a?(Proc)
         @attribute.call(obj)
       else
@@ -36,7 +38,17 @@ module TableBuilderHelper
     end
 
     def link_to(obj)
+      return unless check_condition(obj)
       @link_to.call(obj)
+    end
+
+    def check_condition(obj)
+      condition_val = true
+      if @condition.present?
+        condition_val = @condition
+        condition_val = condition_val.call(obj) if condition_val.is_a?(Proc)
+      end
+      !!condition_val
     end
   end
 
