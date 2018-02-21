5abbd1175af241d4a247452a2c1dd132587fda2e
cedricnjanga <ced.njg@gmail.com>
2018-02-21 06:17:25 -0800

    Refs #5947 Remove POST method from Route#duplicate action link

--- app/decorators/route_decorator.rb
+++ app/decorators/route_decorator.rb
@@ -61,7 +61,6 @@ class RouteDecorator < AF83::Decorator
       policy: :duplicate
     ) do |l|
       l.content h.t('routes.duplicate.title')
-      l.method :post
       l.href do
         h.duplicate_referential_line_route_path(
           context[:referential],
