f6fd7707c0ae9f23769928da6ba180327915e74f
cedricnjanga <ced.njg@gmail.com>
2018-03-06 06:53:04 -0800

    Refs #5934 Remove destructive action links for route stop points in Routes#show

--- app/decorators/stop_point_decorator.rb
+++ app/decorators/stop_point_decorator.rb
@@ -10,26 +10,5 @@ class StopPointDecorator < AF83::Decorator
         )
       end
     end
-
-    instance_decorator.edit_action_link if: ->{ h.policy(Chouette::StopArea).update? } do |l|
-      l.content h.t('stop_points.actions.edit')
-      l.href do
-        h.edit_stop_area_referential_stop_area_path(
-          object.stop_area.stop_area_referential,
-          object.stop_area
-        )
-      end
-    end
-
-    instance_decorator.destroy_action_link if: ->{ h.policy(Chouette::StopArea).destroy? } do |l|
-      l.content h.destroy_link_content('stop_points.actions.destroy')
-      l.href do
-        h.referential_stop_area_path(
-          object.referential,
-          object.stop_area
-        )
-      end
-      l.data confirm: h.t('stop_points.actions.destroy_confirm')
-    end
   end
 end
