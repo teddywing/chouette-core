d8cb2105d28f1463b22b556c1eb0bb4ff2ec3564
cedricnjanga <ced.njg@gmail.com>
2018-03-09 08:46:01 -0800

    Refs #5934 clean spec file

--- spec/features/routes_spec.rb
+++ spec/features/routes_spec.rb
@@ -6,7 +6,6 @@ describe "Routes", :type => :feature do
   let!(:route2) { create(:route, :line => line) }
   #let!(:stop_areas) { Array.new(4) { create(:stop_area) } }
   let!(:stop_points) { Array.new(4) { create(:stop_point, :route => route) } }
-  let(:stop_area_referential) {stop_points.first.stop_area.stop_area_referential}
   let!(:journey_pattern) { create(:journey_pattern, route: route) }
 
   before { @user.update(organisation: referential.organisation) }
@@ -87,16 +86,6 @@ describe "Routes", :type => :feature do
           @user.update_attribute(:permissions, [])
           visit referential_line_route_path(referential, line, route)
           expect(page).not_to have_link(I18n.t('actions.destroy'), href: referential_line_route_journey_pattern_path(referential, line, route, journey_pattern))
-        end
-      end
-
-      context 'user has permission to destroy journey patterns' do
-        xit 'shows destroy links for journey patterns' do
-          @user.update_attribute(:permissions, ["stop_areas.destroy"])
-          stop_points.each_with_index do |sp, i|
-            stop_point_action_links_btn(i + 1).click
-            expect(page).to have_link(I18n.t('stop_points.actions.destroy'), href: stop_area_referential_stop_area_path(sp.referential, sp))
-          end
         end
       end
     end
