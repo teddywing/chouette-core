908128461e185f7fbf65f2e17ae478b6df123fb8
Teddy Wing <teddy.wing@af83.com>
2018-03-12 11:46:03 +0100

    Revert "TomTom: Add method for `calculateRoute` endpoint"
    This reverts commit f28a4b2c5b348bc12b455aa0cd76a9513103aea7.
    
    As stated in that commit, I'm going to use the `/batch` endpoint instead
    of `/calculateRoute` because I know that's already working.

--- lib/tom_tom.rb
+++ lib/tom_tom.rb
@@ -14,19 +14,6 @@ class TomTom
     end
   end
 
-  def calculate_route(way_costs)
-    params = URI.encode_www_form({
-      travelMode: 'bus',
-      routeType: 'shortest'
-    })
-    locations = convert_way_costs_for_calculate_route(way_costs)
-
-    response = @connection.post do |req|
-      req.url "/routing/1/calculateRoute/#{locations}/json?#{params}"
-      req.headers['Content-Type'] = 'application/json'
-    end
-  end
-
   def batch(way_costs)
     params = URI.encode_www_form({
       travelMode: 'bus',
@@ -45,16 +32,6 @@ class TomTom
         batchItems: batch_items
       }.to_json
     end
-
-    response = JSON.parse(response.body)
-
-    calculated_routes = response['batchItems']
-
-    calculated_routes.each do |route|
-      next if route['statusCode'] != 200
-
-      distance = route['response']['routes']
-    end
   end
 
   def convert_way_costs_for_batch(way_costs)
@@ -62,18 +39,5 @@ class TomTom
       "#{way_cost.departure.lat},#{way_cost.departure.lng}" \
       ":#{way_cost.arrival.lat},#{way_cost.arrival.lng}"
     end
-  end
-
-  def convert_way_costs_for_calculate_route(way_costs)
-    coordinates = []
-
-    way_costs.map do |way_cost|
-      coordinates << "#{way_cost.departure.lat},#{way_cost.departure.lng}"
-      coordinates << "#{way_cost.arrival.lat},#{way_cost.arrival.lng}"
-    end
-
-    coordinates
-      .uniq
-      .join(':')
   end
 end
