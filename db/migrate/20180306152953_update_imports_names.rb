921d23bca18fec64fd40b393e1e63bfea3184506
Luc Donnet <luc.donnet@af83.com>
2018-03-12 16:49:47 +0100

    Fix migration 20180306152953_update_imports_names with compact Refs #6133 @1

--- db/migrate/20180306152953_update_imports_names.rb
+++ db/migrate/20180306152953_update_imports_names.rb
@@ -5,7 +5,7 @@ class UpdateImportsNames < ActiveRecord::Migration
       Import::Base.where(type: type).update_all type: "Import::#{type.gsub 'Import', ''}"
     end
 
-    Import::Base.all.pluck(:parent_type).uniq.each do |type|
+    Import::Base.all.pluck(:parent_type).uniq.compact.each do |type|
       next if type =~ /^Import/
       Import::Base.where(parent_type: type).update_all parent_type: "Import::#{type.gsub 'Import', ''}"
     end
