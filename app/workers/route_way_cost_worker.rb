13135a5251f2af3e85bb2bb1be9866477b7414e9
Teddy Wing <teddy.wing@af83.com>
2018-03-13 18:10:46 +0100

    RouteWayCostWorker: Fix recursive worker call
    Since this method was run on `after_save` and in it
    `RouteWayCostCalculator` calls `#update` on the given route, it caused
    an infinite recursive call to the worker. To prevent this, wrap the
    `#calculate!` call in methods that unset and reset the callback.
    
    Refs #6095

--- app/workers/route_way_cost_worker.rb
+++ app/workers/route_way_cost_worker.rb
@@ -4,6 +4,11 @@ class RouteWayCostWorker
   def perform(referential_id, route_id)
     Referential.find(referential_id).switch
     route = Chouette::Route.find(route_id)
+
+    Chouette::Route.skip_callback(:save, :after, :calculate_costs!)
+
     RouteWayCostCalculator.new(route).calculate!
+
+    Chouette::Route.set_callback(:save, :after, :calculate_costs!)
   end
 end
