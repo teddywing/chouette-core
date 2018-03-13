96d01a90977de5d94ab3f5c31e043579c86b150a
Teddy Wing <teddy.wing@af83.com>
2018-03-13 18:08:16 +0100

    RouteWayCostWorker: Switch to referential in order to find Route
    Just running `Chouette::Route.find(id)` doesn't give us anything if we
    don't have a `Referential` selected. In order to properly get the
    correct route, pass the referential ID to the worker and switch to that
    referential before trying to select the route.
    
    Refs #6095

--- app/workers/route_way_cost_worker.rb
+++ app/workers/route_way_cost_worker.rb
@@ -1,7 +1,8 @@
 class RouteWayCostWorker
   include Sidekiq::Worker
 
-  def perform(route_id)
+  def perform(referential_id, route_id)
+    Referential.find(referential_id).switch
     route = Chouette::Route.find(route_id)
     RouteWayCostCalculator.new(route).calculate!
   end
