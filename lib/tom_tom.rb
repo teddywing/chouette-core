c0eac353d764f97b6845a0defad5ded1824549b7
Teddy Wing <teddy.wing@af83.com>
2018-03-15 11:52:16 +0100

    Route: Don't run `#calculate_costs!` on callback if TomTom disabled
    We say `TomTom` is disabled when no API key is present. If this is the
    case, the `after_save` callback that uses it shouldn't be executed.
    
    I had to change my `API_KEY` constant to an instance variable to be able
    to change it for testing.
    
    Refs #6095

--- lib/tom_tom.rb
+++ lib/tom_tom.rb
@@ -1,15 +1,19 @@
 module TomTom
   BASE_URL = 'https://api.tomtom.com'
-  API_KEY = Rails.application.secrets.tomtom_api_key
+  @api_key = Rails.application.secrets.tomtom_api_key
 
   @connection = Faraday.new(
     url: BASE_URL,
     params: {
-      key: API_KEY
+      key: @api_key
     }
   ) do |faraday|
     faraday.use FaradayMiddleware::FollowRedirects, limit: 1
     faraday.adapter Faraday.default_adapter
+  end
+
+  def self.enabled?
+    @api_key.present?
   end
 
   def self.batch(way_costs)
