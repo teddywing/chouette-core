c297e8a97f41221c0a6cfd5f3aee89589811d47c
cedricnjanga <ced.njg@gmail.com>
2018-03-15 06:57:35 -0700

    Refs #6139 Change Min max values validation concern to allowed float values

--- app/models/concerns/min_max_values_validation.rb
+++ app/models/concerns/min_max_values_validation.rb
@@ -2,12 +2,21 @@ module MinMaxValuesValidation
   extend ActiveSupport::Concern
 
   included do
+    before_validation :force_min_max_values_to_respect_numeric_format
     validates_presence_of :minimum, :maximum
+    validates_numericality_of :minimum, :maximum, allow_nil: true, greater_than_or_equal_to: 0
     validate :min_max_values_validation
   end
 
+  def force_min_max_values_to_respect_numeric_format
+    if self.minimum && self.maximum
+      self.minimum = self.minimum.gsub(",", ".")
+      self.maximum = self.maximum.gsub(",", ".")
+    end
+  end
+
   def min_max_values_validation
-    return true if (minimum && maximum) && (minimum.to_i < maximum.to_i)
+    return true if (maximum && maximum) && (minimum.to_f < maximum.to_f)
     errors.add(:minimum, I18n.t('compliance_controls.min_max_values', min: minimum, max: maximum))
   end
 end
--- app/models/generic_attribute_control/min_max.rb
+++ app/models/generic_attribute_control/min_max.rb
@@ -1,9 +1,7 @@
 module GenericAttributeControl
   class MinMax < ComplianceControl
     store_accessor :control_attributes, :minimum, :maximum, :target
-
-    validates_numericality_of :minimum, allow_nil: true, greater_than_or_equal_to: 0
-    validates_numericality_of :maximum, allow_nil: true, greater_than_or_equal_to: 0
+    
     validates :target, presence: true
     include MinMaxValuesValidation
 
