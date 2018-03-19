ab80649830a6d6aa0ffe12c16b3bac60249528f7
cedricnjanga <ced.njg@gmail.com>
2018-03-18 21:23:14 -0700

    Refs #6139 Add format validation on min max values for compliance controls

--- app/models/concerns/min_max_values_validation.rb
+++ app/models/concerns/min_max_values_validation.rb
@@ -2,17 +2,10 @@ module MinMaxValuesValidation
   extend ActiveSupport::Concern
 
   included do
-    before_validation :force_min_max_values_to_respect_numeric_format
     validates_presence_of :minimum, :maximum
     validates_numericality_of :minimum, :maximum, allow_nil: true, greater_than_or_equal_to: 0
+    validates_format_of :minimum, :maximum, with: %r{\A\d+\.\d{2}\Z}
     validate :min_max_values_validation
-  end
-
-  def force_min_max_values_to_respect_numeric_format
-    if self.minimum && self.maximum
-      self.minimum = self.minimum.gsub(",", ".")
-      self.maximum = self.maximum.gsub(",", ".")
-    end
   end
 
   def min_max_values_validation
