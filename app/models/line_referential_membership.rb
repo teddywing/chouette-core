e39d3e055ff61aebe4235492059c6bb7cb1d956f
Alban Peignier <alban.peignier@af83.com>
2018-03-26 15:16:22 +0200

    Only create new membership when organisation isn't present in #add_member. Refs #6328

--- app/models/line_referential_membership.rb
+++ app/models/line_referential_membership.rb
@@ -1,4 +1,7 @@
 class LineReferentialMembership < ActiveRecord::Base
   belongs_to :organisation
   belongs_to :line_referential
+
+  validates :organisation_id, presence: true, uniqueness: { scope: :line_referential }
+  validates :line_referential_id, presence: true
 end
