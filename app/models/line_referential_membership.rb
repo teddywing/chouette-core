501319849f6ad6c058d062df458df3f7838fc634
Alban Peignier <alban.peignier@af83.com>
2018-03-30 14:25:51 +0200

    Remove line/stop_area_referential_id validation in ReferentialMemberships. Refs #6328

--- app/models/line_referential_membership.rb
+++ app/models/line_referential_membership.rb
@@ -3,5 +3,4 @@ class LineReferentialMembership < ActiveRecord::Base
   belongs_to :line_referential
 
   validates :organisation_id, presence: true, uniqueness: { scope: :line_referential }
-  validates :line_referential_id, presence: true
 end
--- app/models/stop_area_referential_membership.rb
+++ app/models/stop_area_referential_membership.rb
@@ -3,5 +3,4 @@ class StopAreaReferentialMembership < ActiveRecord::Base
   belongs_to :stop_area_referential
 
   validates :organisation_id, presence: true, uniqueness: { scope: :stop_area_referential }
-  validates :stop_area_referential_id, presence: true
 end
