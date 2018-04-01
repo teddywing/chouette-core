ac8587140a44a197ec3b30435a2194bf8b7f0e7c
Alban Peignier <alban.peignier@af83.com>
2018-04-01 20:20:32 +0200

    Create Import::Gtfs directly into Import::Workbench when GTFS file is detected. Refs #6368

--- app/models/import/workbench.rb
+++ app/models/import/workbench.rb
@@ -2,6 +2,25 @@ class Import::Workbench < Import::Base
   after_commit :launch_worker, :on => :create
 
   def launch_worker
-    WorkbenchImportWorker.perform_async(id)
+    unless Import::Gtfs.accept_file?(file.path)
+      WorkbenchImportWorker.perform_async(id)
+    else
+      import_gtfs
+    end
+  end
+
+  def import_gtfs
+    update_column :status, 'running'
+    update_column :started_at, Time.now
+
+    Import::Gtfs.create! parent_id: self.id, workbench: workbench, file: File.new(file.path), name: "Import GTFS", creator: "Web service"
+
+    update_column :status, 'successful'
+    update_column :ended_at, Time.now
+  rescue Exception => e
+    Rails.logger.error "Error while processing GTFS file: #{e}"
+
+    update_column :status, 'failed'
+    update_column :ended_at, Time.now
   end
 end
