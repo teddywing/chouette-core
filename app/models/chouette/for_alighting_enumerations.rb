95172d1a3b00f58cb57d39f3fc6fc6c5851c40ec
Alban Peignier <alban.peignier@af83.com>
2018-04-03 09:22:29 +0200

    Set default for enumerizes :for_boarding and :for_alighting. Refs #6387

--- app/models/chouette/for_alighting_enumerations.rb
+++ app/models/chouette/for_alighting_enumerations.rb
@@ -3,6 +3,6 @@ module Chouette
     extend Enumerize
     extend ActiveModel::Naming
 
-    enumerize :for_alighting, in: %w[normal forbidden request_stop is_flexible]
+    enumerize :for_alighting, in: %w[normal forbidden request_stop is_flexible], default: :normal
   end
 end
