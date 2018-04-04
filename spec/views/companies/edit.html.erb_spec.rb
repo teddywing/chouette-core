278b4c107054c6c9213473a943ddeae8585aff41
Zog <johan.van-ryseghem@af83.com>
2018-04-04 09:07:24 +0200

    Fix more specs

--- spec/views/companies/edit.html.erb_spec.rb
+++ spec/views/companies/edit.html.erb_spec.rb
@@ -5,7 +5,10 @@ describe "/companies/edit", :type => :view do
   let!(:company) { assign(:company, create(:company)) }
   let!(:companies) { Array.new(2) { create(:company) } }
   let!(:line_referential) { assign :line_referential, company.line_referential }
-
+  before do
+    allow(view).to receive(:resource){ company }
+    allow(view).to receive(:current_referential){ first_referential }
+  end
   describe "form" do
     it "should render input for name" do
       render
