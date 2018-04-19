682fc27aad1027850cdb77b5c9720914bb84ca56
Zog <johan.van-ryseghem@af83.com>
2018-04-19 16:59:19 +0200

    Refs #6572; Update policies
    disable all actions for non-ready referentials

--- app/policies/referential_policy.rb
+++ app/policies/referential_policy.rb
@@ -14,32 +14,36 @@ class ReferentialPolicy < ApplicationPolicy
   end
 
   def destroy?
-    !referential_read_only? && organisation_match? && user.has_permission?('referentials.destroy')
+    referential_ready? && !referential_read_only? && organisation_match? && user.has_permission?('referentials.destroy')
   end
 
   def update?
-    !referential_read_only? && organisation_match? && user.has_permission?('referentials.update')
+    referential_ready? && !referential_read_only? && organisation_match? && user.has_permission?('referentials.update')
   end
 
   def clone?
-    !record.in_referential_suite? && create?
+    referential_ready? && !record.in_referential_suite? && create?
   end
 
   def validate?
-    !referential_read_only? && create? && organisation_match?
+    referential_ready? && !referential_read_only? && create? && organisation_match?
   end
 
   def archive?
-    !referential_read_only? && record.archived_at.nil? && organisation_match? && user.has_permission?('referentials.update')
+    referential_ready? && !referential_read_only? && record.archived_at.nil? && organisation_match? && user.has_permission?('referentials.update')
   end
 
   def unarchive?
-    record.archived? && !record.merged? && organisation_match? && user.has_permission?('referentials.update')
+    referential_ready? && record.archived? && !record.merged? && organisation_match? && user.has_permission?('referentials.update')
   end
 
   def common_lines?
     # TODO: Replace with correct BL ASA available, c.f. https://projects.af83.io/issues/2692
     true
+  end
+
+  def referential_ready?
+    record.ready?
   end
 
 end
