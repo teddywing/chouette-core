199e4eaeffe8a826f5215359494ba3cc6ac6eb8a
Alban Peignier <alban.peignier@af83.com>
2018-04-25 09:45:10 +0200

    Move workgroup method to less global Class (TridentActiveRecord/LineReferentialSupport and StopAreaReferentialSupport) to avoid to overide existing methods. Refs #6669

--- app/models/application_model.rb
+++ app/models/application_model.rb
@@ -2,20 +2,4 @@ class ApplicationModel < ::ActiveRecord::Base
   include MetadataSupport
 
   self.abstract_class = true
-
-  def self.referential
-    Referential.where(slug: Apartment::Tenant.current).last
-  end
-
-  def referential
-    self.class.referential
-  end
-
-  def self.workgroup
-    referential&.workgroup
-  end
-
-  def workgroup
-    self.class.workgroup
-  end
 end
