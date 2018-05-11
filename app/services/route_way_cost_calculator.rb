eec9a97dc17ce57d3153e918a72b0ef74fb23af9
Teddy Wing <teddy.wing@af83.com>
2018-05-11 11:58:55 +0200

    RouteWayCostCalculator: Filter costs through WayCostMinimumDistance
    When calculating `WayCost`s, run them through this class which will snap
    short distances (under 500 metres) to a cost of 1 kilometre and 1
    minute.
    
    Refs #6952

--- app/services/route_way_cost_calculator.rb
+++ app/services/route_way_cost_calculator.rb
@@ -6,6 +6,7 @@ class RouteWayCostCalculator
   def calculate!
     way_costs = StopAreasToWayCostsConverter.new(@route.stop_areas).convert
     way_costs = TomTom.matrix(way_costs)
+    way_costs = WayCostMinimumDistance.new(way_costs).snap_short_costs_to_1
     way_costs = WayCostCollectionJSONSerializer.dump(way_costs)
     @route.update(costs: way_costs)
   end
