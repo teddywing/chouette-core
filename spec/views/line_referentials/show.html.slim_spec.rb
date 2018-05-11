dcede6f1d323c827ae9e663b312ab32f2037b525
Zog <johan.van-ryseghem@af83.com>
2018-05-11 14:55:30 +0200

    Refs #6647; Fix specs

--- spec/views/line_referentials/show.html.slim_spec.rb
+++ spec/views/line_referentials/show.html.slim_spec.rb
@@ -2,20 +2,26 @@ require 'spec_helper'
 
 describe "/line_referentials/show", :type => :view do
 
-  let!(:line_referential) { assign :line_referential, create(:line_referential) }
-
-  before :each do
-    render
+  let(:line_referential) do
+    line_referential = create(:line_referential)
+    assign :line_referential, line_referential.decorate
   end
 
-  # FIXME See #6647
-  # it "should not present syncing infos and button" do
-  #   expect(view.content_for(:page_header_actions)).to_not have_selector("a[href=\"#{view.sync_line_referential_path(line_referential)}\"]")
-  # end
+  before :each do
+    controller.request.path_parameters[:id] = line_referential.id
+    allow(view).to receive(:params).and_return({action: :show})
+    allow(view).to receive(:resource).and_return(line_referential)
 
-  # with_permission "line_referentials.synchronize" do
-  #   it "should present syncing infos and button" do
-  #     expect(view.content_for(:page_header_actions)).to have_selector("a[href=\"#{view.sync_line_referential_path(line_referential)}\"]", count: 1)
-  #   end
-  # end
+    render  template: "line_referentials/show", layout: "layouts/application"
+  end
+
+  it "should not present syncing infos and button" do
+    expect(rendered).to_not have_selector("a[href=\"#{view.sync_line_referential_path(line_referential)}\"]")
+  end
+
+  with_permission "line_referentials.synchronize" do
+    it "should present syncing infos and button" do
+      expect(rendered).to have_selector("a[href=\"#{view.sync_line_referential_path(line_referential)}\"]", count: 1)
+    end
+  end
 end
--- spec/views/stop_area_referentials/show.html.slim_spec.rb
+++ spec/views/stop_area_referentials/show.html.slim_spec.rb
@@ -2,20 +2,25 @@ require 'spec_helper'
 
 describe "/stop_area_referentials/show", :type => :view do
 
-  let!(:stop_area_referential) { assign :stop_area_referential, create(:stop_area_referential) }
-
-  before :each do
-    render
+  let(:stop_area_referential) do
+    stop_area_referential = create(:stop_area_referential)
+    assign :stop_area_referential, stop_area_referential.decorate
   end
 
-  # FIXME See #6647
-  # it "should not present syncing infos and button" do
-  #   expect(view.content_for(:page_header_actions)).to_not have_selector("a[href=\"#{view.sync_stop_area_referential_path(stop_area_referential)}\"]")
-  # end
+  before :each do
+    controller.request.path_parameters[:id] = stop_area_referential.id
+    allow(view).to receive(:params).and_return({action: :show})
+    allow(view).to receive(:resource).and_return(stop_area_referential)
+    render template: "stop_area_referentials/show", layout: "layouts/application"
+  end
 
-  # with_permission "stop_area_referentials.synchronize" do
-  #   it "should present syncing infos and button" do
-  #     expect(view.content_for(:page_header_actions)).to have_selector("a[href=\"#{view.sync_stop_area_referential_path(stop_area_referential)}\"]", count: 1)
-  #   end
-  # end
+  it "should not present syncing infos and button" do
+    expect(rendered).to_not have_selector("a[href=\"#{view.sync_stop_area_referential_path(stop_area_referential)}\"]")
+  end
+
+  with_permission "stop_area_referentials.synchronize" do
+    it "should present syncing infos and button" do
+      expect(rendered).to have_selector("a[href=\"#{view.sync_stop_area_referential_path(stop_area_referential)}\"]", count: 1)
+    end
+  end
 end
