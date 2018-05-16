7369847983d5753483f008b6b64949def2a0ef84
Luc Donnet <luc.donnet@af83.com>
2018-05-16 09:21:01 +0200

    Hack to fix migration 20180412095756_add_referentials_to_import_resources.rb. Need to test if the column exist before add it to table. Refs #6991 @1

--- db/migrate/20180412095756_add_referentials_to_import_resources.rb
+++ db/migrate/20180412095756_add_referentials_to_import_resources.rb
@@ -1,5 +1,5 @@
 class AddReferentialsToImportResources < ActiveRecord::Migration
   def change
-    add_reference :import_resources, :referential, type: :bigint, index: true, foreign_key: true
+    add_reference :import_resources, :referential, type: :bigint, index: true, foreign_key: true unless column_exists? :import_resources, :referential_id
   end
 end
