70d55a1a62a2207453b738cb81003f57c537473c
Alban Peignier <alban.peignier@af83.com>
2018-05-16 17:00:41 +0200

    Update RouteWayCostCalculator spec after Faraday. Refs #7032

--- spec/services/route_way_cost_calculator_spec.rb
+++ spec/services/route_way_cost_calculator_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe RouteWayCostCalculator do
             'Accept'=>'*/*',
             'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
             'Content-Type'=>'application/json',
-            'User-Agent'=>'Faraday v0.9.2'
+            'User-Agent'=>'Faraday v0.15.1'
           })
         .to_return(
           status: 200,
