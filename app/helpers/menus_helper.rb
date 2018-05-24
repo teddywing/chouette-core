c1ebc713fd0eb4692d679fc80e184cb802c98602
Zog <johan.van-ryseghem@af83.com>
2018-05-24 12:57:16 +0200

    Refs #7118; Fix menus markup

--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -10,7 +10,7 @@ module MenusHelper
         end
       end
       out += content_tag(:div, class: "panel-collapse collapse", id: "menu-item-#{@current_menu_item_count}") do
-        content_tag :li, class: "list-group" do
+        content_tag :div, class: "list-group" do
           capture(&block)
         end
       end
@@ -18,21 +18,3 @@ module MenusHelper
     end
   end
 end
-
-# .menu-item.panel
-#   .panel-heading
-#     h4.panel-title
-#       = link_to '#miOne', data: {toggle: 'collapse', parent: '#menu-items'}, 'aria-expanded' => 'false' do
-#         = t('layouts.navbar.current_offer.other')
-#
-#   #miOne.panel-collapse.collapse
-#     .list-group
-#       = link_to root_path, class: "list-group-item" do
-#         span = t('layouts.navbar.dashboard')
-#       = link_to workbench_output_path(workbench), class: 'list-group-item' do
-#         span = t('layouts.navbar.workbench_outputs.organisation')
-#       = link_to '#', class: 'list-group-item disabled' do
-#         span = t('layouts.navbar.workbench_outputs.workgroup')
-#       - if policy(workbench.workgroup).edit?
-#         = link_to [:edit, workbench.workgroup], class: 'list-group-item' do
-#           span = t('layouts.navbar.workbench_outputs.edit_workgroup')
