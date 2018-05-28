80747059020a86ab4bd3f564a8c9a8cfabd2b5c5
Zog <johan.van-ryseghem@af83.com>
2018-05-28 09:07:23 +0200

    Refs #7159; Fix specs

--- spec/controllers/exports_controller_spec.rb
+++ spec/controllers/exports_controller_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe ExportsController, :type => :controller do
         name: "foo",
         type: "Export::Netex",
         duration: 12,
-        export_type: :line,
+        export_subtype: :full,
         referential_id: first_referential.id
       }}
 
--- spec/factories/exports/netex_exports.rb
+++ spec/factories/exports/netex_exports.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :netex_export, class: Export::Netex, parent: :export do
     association :parent, factory: :workgroup_export
-    export_type :line
+    export_subtype :line
     duration 90
   end
 end
